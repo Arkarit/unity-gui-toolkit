dc6ec29c3db10e05d5869e25bc10b926ef56fded
Heiko Achilles <heiko.achilles@innogames.com>
2020-01-09 05:43:37 +0100

    Split UiGradient into base class

---
+++ Code/Basic/UiGradientBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	[ExecuteAlways]
+	public abstract class UiGradientBase : BaseMeshEffect
+	{
+		public bool m_byUV = true;
+
+		protected static UIVertex s_vertex;
+
+		protected Vector2 m_min;
+		protected Vector2 m_max;
+
+		protected virtual void Prepare( VertexHelper _vh ) {}
+
+		protected abstract Color GetColor( Vector2 _normVal );
+
+		public override void ModifyMesh( VertexHelper _vh )
+		{
+			if (!m_byUV)
+				CalcMinMax( _vh );
+
+			Vector2 dist = m_max - m_min;
+			_vh.PopulateUIVertex(ref s_vertex, 0);
+
+			Prepare( _vh );
+
+
+			for (int i = 0; i < _vh.currentVertCount; ++i)
+			{
+				_vh.PopulateUIVertex(ref s_vertex, i);
+
+				Vector2 lerpVal;
+				if (m_byUV)
+				{
+					Vector2 uv = s_vertex.uv0;
+					lerpVal	= uv;
+				}
+				else
+				{
+					Vector2 pos = new Vector2( s_vertex.position.x, s_vertex.position.y );
+					lerpVal = ( pos-m_min) / dist;
+				}
+
+				s_vertex.color = GetColor(lerpVal);
+
+				_vh.SetUIVertex(s_vertex, i);
+			}
+		}
+
+		private void CalcMinMax( VertexHelper _vh )
+		{
+			m_min = new Vector2( float.MaxValue, float.MaxValue );
+			m_max = new Vector2( -float.MaxValue, -float.MaxValue );
+
+			for (int i = 0; i < _vh.currentVertCount; ++i)
+			{
+				_vh.PopulateUIVertex(ref s_vertex, i);
+
+				m_min.x = Mathf.Min(m_min.x, s_vertex.position.x);
+				m_min.y = Mathf.Min(m_min.y, s_vertex.position.y);
+				m_max.x = Mathf.Max(m_max.x, s_vertex.position.x);
+				m_max.y = Mathf.Max(m_max.y, s_vertex.position.y);
+			}
+		}
+
+#if UNITY_EDITOR
+		protected override void OnValidate()
+		{
+			this.SetDirty();
+		}
+#endif
+	}
+}
---
+++ Code/Basic/UiGradientSimple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	[ExecuteAlways]
+	public class UiGradientSimple : UiGradientBase
+	{
+		public Color m_colorRightOrTop;
+		public Color m_colorLeftOrBottom;
+		public bool m_isHorizontal;
+
+		private Color m_colorA;
+		private Color m_colorB;
+
+		protected override void Prepare( VertexHelper _vh )
+		{
+			_vh.PopulateUIVertex(ref s_vertex, 0);
+			m_colorA = m_colorRightOrTop * s_vertex.color;
+			m_colorB = m_colorLeftOrBottom * s_vertex.color;
+		}
+
+		protected override Color GetColor( Vector2 _normVal )
+		{
+			return Color.Lerp( m_colorA, m_colorB, m_isHorizontal ? _normVal.x : _normVal.y );
+		}
+	}
+}
