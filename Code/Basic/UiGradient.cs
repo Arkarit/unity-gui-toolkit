a14b28430587c763e23f1b4964599d6ddb049ffc
phoenix <phoenix@phoenixgrafik.de>
2020-01-11 03:45:54 +0100

    Introduced UiGradient modifier with real gradient

---
+++ Code/Basic/UiGradient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	[ExecuteAlways]
+	public class UiGradient : UiGradientBase
+	{
+		public Gradient m_gradient = new Gradient();
+		public bool m_isHorizontal;
+
+		protected override Color GetColor( Vector2 _normVal )
+		{
+			return m_gradient.Evaluate( m_isHorizontal ? _normVal.x : 1.0f - _normVal.y );
+		}
+	}
+}
