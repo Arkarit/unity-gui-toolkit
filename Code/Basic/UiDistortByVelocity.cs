41beea4d98b39af62c17d95577b6c82e56d74cf4
phoenix <phoenix@phoenixgrafik.de>
2020-01-25 05:52:35 +0100

    UiDistortByVelocity heavy WIP

--- Code/Basic/UiDistortByVelocity.cs
+++ Code/Basic/UiDistortByVelocity.cs
@@ -6,20 +6,22 @@ using UnityEditor;
 
 namespace GuiToolkit
 {
-	[ExecuteAlways]
 	public class UiDistortByVelocity : UiDistortBase, IEditorUpdateable
 	{
-
+		[SerializeField]
+		private EDirection m_direction = EDirection.Horizontal;
 
 		[SerializeField]
 		[Range(0,1)]
 		private float m_velocityDampen = 0.9f;
 
-		private float m_velocity;
 		private float m_velocityChange;
+
+		private float m_lastVelocity;
 		private Vector2 m_lastPosition;
 
 #if UNITY_EDITOR
+		[System.NonSerialized]
 		public bool m_updating;
 
 		public bool RemoveFromEditorUpdate()
@@ -38,10 +40,47 @@ namespace GuiToolkit
 			Update(Time.deltaTime);
 		}
 
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			m_lastPosition = m_rectTransform.anchoredPosition;
+			m_lastVelocity = 0;
+		}
+
 		private void Update( float _deltaTime )
 		{
-			Debug.Log($"m_velocity; {m_velocity} m_velocityChange: {m_velocityChange}");
+#if UNITY_EDITOR
+			if (ShouldSkipUpdate())
+				return;
+#endif
+			if (_deltaTime == 0)
+				return;
+
+			m_velocityChange *= m_velocityDampen;
+
+			Vector2 movementV2 = m_rectTransform.anchoredPosition - m_lastPosition;
+			m_lastPosition = m_rectTransform.anchoredPosition;
+
+			float movement = m_direction == EDirection.Horizontal ? movementV2.x : movementV2.y;
+
+			float velocity = movement / _deltaTime;
+			m_velocityChange -= velocity - m_lastVelocity;
+			m_lastVelocity = velocity;
+
+if (Mathf.Abs(m_velocityChange) > 0.0001)
+Debug.Log($"m_lastVelocity: {m_lastVelocity} m_velocityChange: {m_velocityChange}");
 		}
+
+#if UNITY_EDITOR
+		private bool ShouldSkipUpdate()
+		{
+			if (Application.isPlaying)
+				return false;
+			if (m_updating)
+				return false;
+			return true;
+		}
+#endif
 
 	}
 
@@ -49,7 +88,17 @@ namespace GuiToolkit
 	[CustomEditor(typeof(UiDistortByVelocity))]
 	public class UiDistortByVelocityEditor : UiDistortEditorBase
 	{
+		protected SerializedProperty m_directionProp;
+		protected SerializedProperty m_velocityDampenProp;
+
 		private static bool s_updateInEditor = false;
+
+
+		public override void OnEnable()
+		{
+			m_directionProp = serializedObject.FindProperty("m_direction");
+			m_velocityDampenProp = serializedObject.FindProperty("m_velocityDampen");
+		}
 
 		protected override void Edit( UiDistortBase _thisUiDistort )
 		{
@@ -73,15 +122,8 @@ namespace GuiToolkit
 			float oldLabelWidth = EditorGUIUtility.labelWidth;
 			EditorGUIUtility.labelWidth = 100;
 
-// 			EditorGUILayout.BeginHorizontal();
-// 			EditorGUILayout.PropertyField(m_topLeftProp);
-// 			EditorGUILayout.PropertyField(m_topRightProp);
-// 			EditorGUILayout.EndHorizontal();
-// 
-// 			EditorGUILayout.BeginHorizontal();
-// 			EditorGUILayout.PropertyField(m_bottomLeftProp);
-// 			EditorGUILayout.PropertyField(m_bottomRightProp);
-// 			EditorGUILayout.EndHorizontal();
+ 			EditorGUILayout.PropertyField(m_directionProp);
+ 			EditorGUILayout.PropertyField(m_velocityDampenProp);
 
 			EditorGUIUtility.labelWidth = oldLabelWidth;
 		}
