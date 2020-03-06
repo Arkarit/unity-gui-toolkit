c195112d596e7d578b39f8d800803c60e55762a5
phoenix <phoenix@phoenixgrafik.de>
2020-03-06 16:08:46 +0100

    FPS counter for debug purposes

---
+++ Code/Components/Debug/UiFPS.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	public class UiFPS : MonoBehaviour
+	{
+		public int NumSamples = 20;
+		public Text m_text;
+		private float[] m_samples;
+		private int m_idx;
+
+		private void Start()
+		{
+			m_samples = new float[NumSamples];
+		}
+
+		private void Update()
+		{
+			float dt = Time.deltaTime;
+			m_samples[m_idx++] = dt;
+			if (m_idx >= NumSamples)
+				m_idx = 0;
+
+			float sum = 0;
+			for (int i=0; i<NumSamples; i++)
+				sum += m_samples[i];
+			sum /= NumSamples;
+			sum = 1.0f / sum;
+			m_text.text = ((int)sum).ToString();
+		}
+	}
+}
