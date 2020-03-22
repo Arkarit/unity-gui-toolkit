6e4b8b5fac096fdbcd741d8f608d483a900428e2
Heiko Achilles <heiko.achilles@innogames.com>
2020-03-22 18:55:31 +0100

    Stubs for new layout groups. Just an experimental branch for now; don't know if I will continue this

---
+++ Code/Components/UiLayout.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	public class UiLayout : LayoutGroup
+	{
+		public enum EFitMode
+		{
+			Fix,
+			ShrinkWrap,
+		}
+
+		[SerializeField]
+		protected int m_maxHorizontal = Constants.INVALID;
+
+		[SerializeField]
+		protected int m_maxVertical = 1;
+
+		[SerializeField]
+		protected EFitMode m_selfSizeHorizontal = EFitMode.Fix;
+
+		[SerializeField]
+		protected EFitMode m_selfSizeVertical = EFitMode.Fix;
+
+        [SerializeField]
+		protected float m_spacing = 0;
+
+
+        public int MaxHorizontal { get { return m_maxHorizontal; } set { SetProperty(ref m_maxHorizontal, value); } }
+        public int MaxVertical { get { return m_maxVertical; } set { SetProperty(ref m_maxVertical, value); } }
+        public float Spacing { get { return m_spacing; } set { SetProperty(ref m_spacing, value); } }
+        public EFitMode SelfSizeHorizontal { get { return m_selfSizeHorizontal; } set { SetProperty(ref m_selfSizeHorizontal, value); } }
+        public EFitMode SelfSizeVertical { get { return m_selfSizeVertical; } set { SetProperty(ref m_selfSizeVertical, value); } }
+
+		public override void CalculateLayoutInputVertical()
+		{
+		}
+
+		public override void SetLayoutHorizontal()
+		{
+		}
+
+		public override void SetLayoutVertical()
+		{
+		}
+
+	}
+}
---
+++ Code/Components/UiLayoutElement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	public class UiLayoutElement : UIBehaviour, ILayoutElement, ILayoutIgnorer
+	{
+		public float minWidth => throw new System.NotImplementedException();
+
+		public float preferredWidth => throw new System.NotImplementedException();
+
+		public float flexibleWidth => throw new System.NotImplementedException();
+
+		public float minHeight => throw new System.NotImplementedException();
+
+		public float preferredHeight => throw new System.NotImplementedException();
+
+		public float flexibleHeight => throw new System.NotImplementedException();
+
+		public int layoutPriority => throw new System.NotImplementedException();
+
+		public bool ignoreLayout => throw new System.NotImplementedException();
+
+		public void CalculateLayoutInputHorizontal()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void CalculateLayoutInputVertical()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
