d6798fa13d590eb6c4875788f7317ac789164e6a
Heiko Achilles <heiko.achilles@innogames.com>
2020-04-03 12:16:46 +0200

    Introduced modes for creating color grid Still a lot more modes to do

--- Code/Components/UiColorGridSelect.cs
+++ Code/Components/UiColorGridSelect.cs
@@ -9,6 +9,12 @@ namespace GuiToolkit
 {
 	public class UiColorGridSelect : UiThing
 	{
+		public enum Mode
+		{
+			XHueYBright,
+			XBrightYHue,
+		}
+
 		[SerializeField]
 		protected RectTransform m_colorPatchContainer;
 		[SerializeField]
@@ -16,15 +22,29 @@ namespace GuiToolkit
 		[SerializeField]
 		protected ToggleGroup m_toggleGroup;
 		[SerializeField]
+		protected Mode m_mode;
+		[SerializeField]
 		protected int m_numHorizontal;
 		[SerializeField]
 		protected int m_numVertical;
 		[SerializeField]
 		[Range(0f, 1f)]
-		protected float m_minBrightness = 0.2f;
+		protected float m_minX = 0f;
 		[SerializeField]
 		[Range(0f, 1f)]
-		protected float m_maxBrightness = 0.8f;
+		protected float m_maxX = 1f;
+		[SerializeField]
+		[Range(0f, 1f)]
+		protected float m_minY = 0f;
+		[SerializeField]
+		[Range(0f, 1f)]
+		protected float m_maxY = 1f;
+		[SerializeField]
+		[Range(0f, 1f)]
+		protected float m_fixedValue0 = 1f;
+		[SerializeField]
+		[Range(0f, 1f)]
+		protected float m_fixedValue1 = 1f;
 
 		public Action<Color> OnColorChanged;
 
@@ -34,16 +54,14 @@ namespace GuiToolkit
 		{
 			base.Awake();
 
-			float stepX = (1.0f / (float) m_numHorizontal);
-			float stepY = ((1.0f - m_minBrightness) / (float) m_numVertical);
+			float stepX = ((m_maxX - m_minX) / (float) m_numHorizontal);
+			float stepY = ((m_maxY - m_minY) / (float) m_numVertical);
 
 			for (int y = 0; y < m_numVertical; y++ )
 			{
 				for (int x = 0; x < m_numHorizontal; x++ )
 				{
-					float h = x * stepX;
-					float s = 1.0f;
-					float v = m_minBrightness + y * stepY;
+					(float h, float s, float v) = GetColor(x, y, stepX, stepY);
 					UiColorPatch newPatch = Instantiate(m_colorPatchPrefab);
 					newPatch.Toggle.group  = m_toggleGroup;
 					m_toggleGroup.RegisterToggle(newPatch.Toggle);
@@ -56,6 +74,31 @@ namespace GuiToolkit
 			}
 		}
 
+		private (float h, float s, float v) GetColor( int _x, int _y, float _stepX, float _stepY )
+		{
+			float h,s,v;
+
+			switch (m_mode)
+			{
+				case Mode.XHueYBright:
+					h = m_minX + _x * _stepX;
+					s = m_fixedValue0;
+					v = m_minY + _y * _stepY;
+					break;
+				case Mode.XBrightYHue:
+					h = m_minY + _y * _stepY;
+					s = m_fixedValue0;
+					v = m_minX + _x * _stepX;
+					break;
+				default:
+					Debug.Assert(false);
+					h = s = v = 0;
+					break;
+			}
+
+			return (h,s,v);
+		}
+
 		private void OnPatchSelected( UiColorPatch _patch )
 		{
 			OnColorChanged?.Invoke(_patch.Color);
@@ -66,9 +109,9 @@ namespace GuiToolkit
 			if (m_patches.Count == 0)
 				return;
 
-			// Shitty unity Toggles unregister themselves from the group in OnDisable().
+			// Shitty Unity Toggles unregister themselves from the group in OnDisable().
 			// Thus, if SelectRandom() is called in OnEnable() of another component, the OnEnable()'s of the toggles
-			// may not be called yet, and thus the m_Toggles member of the ToggleGroup may be EMPTY here.
+			// may have not be called yet, and thus the m_Toggles member of the ToggleGroup may be EMPTY here.
 			// Unnecessary to say, that m_Toggles is protected and you have NO option to ask ToggleGroup if its empty or to say CollectToggles() or something.
 			// Workaround: Postpone selection until all shitty Unity enabling and disabling and hassenichgesehn is done.
 			// Again three hours of my life sacrificed to shitty Unity.
