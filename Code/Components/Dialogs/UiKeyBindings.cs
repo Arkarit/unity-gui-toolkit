6a2965c0edfdd517beb841ea4844ef76fd3a3bdd
phoenix <phoenix@phoenixgrafik.de>
2020-04-19 15:56:51 +0200

    Begin implementing key bindings heavy WIP Created new branch to use same branch as winmaze; pushing is more convenient that way.

---
+++ Code/Components/Dialogs/UiKeyBindings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	public class UiKeyBindings : UiRequester
+	{
+	}
+}
