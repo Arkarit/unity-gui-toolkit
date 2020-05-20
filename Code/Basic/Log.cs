6466d5af25d48cdf73c23069e2a5d9370224a967
phoenix <phoenix@phoenixgrafik.de>
2020-05-20 08:59:26 +0200

    Now eventually revealed the mysteries of conditional logging :) https://docs.unity3d.com/Manual/BestPracticeUnderstandingPerformanceInUnity7.html (see bottom of the page). No more #ifdef DEBUG crap.

---
+++ Code/Basic/Log.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace GuiToolkit
+{
+	public static class Log
+	{
+		[Conditional("UNITY_EDITOR")]
+		public static void Editor(string _s)
+		{
+			UnityEngine.Debug.Log(_s);
+		}
+
+		[Conditional("DEBUG")]
+		public static void Debug(string _s)
+		{
+			UnityEngine.Debug.Log(_s);
+		}
+
+		[Conditional("DEBUG_LAYOUT")]
+		public static void Layout(string _s)
+		{
+			UnityEngine.Debug.Log(_s);
+		}
+	}
+}
