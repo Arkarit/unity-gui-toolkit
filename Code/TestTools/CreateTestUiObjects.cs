7425c12e854374883b995134f703a447d126c8eb
phoenix <phoenix@phoenixgrafik.de>
2020-05-21 11:12:37 +0200

    Test helpers for spawning UI objects

---
+++ Code/TestTools/CreateTestUiObjects.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GuiToolkit
+{
+	public class CreateTestUiObjects : MonoBehaviour
+	{
+		public TestUiObject m_prefab;
+		public int m_numInstances;
+
+#if UNITY_EDITOR
+#if TEST_UI
+
+		public void Spawn()
+		{
+			for (int i=0; i<m_numInstances; i++)
+			{
+				GameObject go = PrefabUtility.InstantiatePrefab(m_prefab.gameObject) as GameObject;
+				TestUiObject obj = go.GetComponent<TestUiObject>();
+				obj.transform.SetParent(transform, false);
+				obj.Init(i, m_numInstances);
+			}
+		}
+#endif
+#endif
+	}
+
+#if UNITY_EDITOR
+#if TEST_UI
+	[CustomEditor(typeof(CreateTestUiObjects))]
+	public class CreateTestUiObjectsEditor : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+			if (GUILayout.Button("Create"))
+			{
+				CreateTestUiObjects thisCreateTestUiObjects = target as CreateTestUiObjects;
+				thisCreateTestUiObjects.Spawn();
+			}
+		}
+	}
+#endif
+#endif
+
+}
---
+++ Code/TestTools/TestUiObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace GuiToolkit
+{
+	public class TestUiObject : MonoBehaviour
+	{
+		public TMP_Text m_text;
+
+		public void Init(int _index, int _indexCount)
+		{
+			m_text.text = _index.ToString();
+			float hue = (float) _index / (float) _indexCount;
+			m_text.color = Color.HSVToRGB(hue,1,1);
+		}
+	}
+}
