ed23807a53b0bcab6f170f2a112b7dd420775dca
phoenix <phoenix@phoenixgrafik.de>
2020-12-07 10:33:15 +0100

    Begin improving radial layout group

--- Code/Components/UiRadialLayoutGroup.cs
+++ Code/Components/UiRadialLayoutGroup.cs
@@ -1,20 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace GuiToolkit
 {
 	public class UiRadialLayoutGroup : LayoutGroup
 	{
-		public float m_radius;
+		[SerializeField]
+		private float m_radius;
 
-		[Range(0f, 360f)]
-		public float m_minAngle;
+		[SerializeField]
+		private float m_zIncrement = 0;
 
-		[Range(0f, 360f)]
-		public float m_maxAngle;
+		[SerializeField]
+		private bool m_rotateElements = false;
 
+		[SerializeField]
+		[Range(-360f, 360f)]
+		private float m_rotationAngleOffset;
+
+		[SerializeField]
 		[Range(0f, 360f)]
-		public float m_startAngle;
+		private float m_minAngle;
+
+		[SerializeField]
+		[Range(0f, 360f)]
+		private float m_maxAngle;
+
+		[SerializeField]
+		[Range(0f, 360f)]
+		private float m_startAngle;
+
+		[SerializeField]
+		[HideInInspector]
+		private bool m_useZIncrement;
 
 		protected override void OnEnable()
 		{
@@ -39,35 +61,100 @@ namespace GuiToolkit
 		protected override void OnValidate()
 		{
 			base.OnValidate();
+			m_useZIncrement = !Mathf.Approximately(m_zIncrement, 0);
 			CalculateRadial();
 		}
 #endif
 
-		void CalculateRadial()
+		private void CalculateRadial()
 		{
 			m_Tracker.Clear();
 			if (transform.childCount == 0)
 				return;
-			float fOffsetAngle = ((m_maxAngle - m_minAngle)) / (transform.childCount - 1);
 
-			float fAngle = m_startAngle;
+			float offsetAngle = ((m_maxAngle - m_minAngle)) / (transform.childCount - 1);
+			float angle = m_startAngle;
+
+			int childCount = transform.childCount;
+			float z = 0;
+			if (m_useZIncrement && childCount > 1)
+			{
+				z = - m_zIncrement * childCount / 2;
+			}
+
 			for (int i = 0; i < transform.childCount; i++)
 			{
 				RectTransform child = (RectTransform)transform.GetChild(i);
 				if (child != null)
 				{
-					//Adding the elements to the tracker stops the user from modifiying their positions via the editor.
-					m_Tracker.Add(this, child,
-					DrivenTransformProperties.Anchors |
-					DrivenTransformProperties.AnchoredPosition |
-					DrivenTransformProperties.Pivot);
-					Vector3 vPos = new Vector3(Mathf.Cos(fAngle * Mathf.Deg2Rad), Mathf.Sin(fAngle * Mathf.Deg2Rad), 0);
+					m_Tracker.Add( this, child, GetDrivenTransformProperties() );
+					Vector3 vPos = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0);
 					child.localPosition = vPos * m_radius;
-					//Force objects to be center aligned, this can be changed however I'd suggest you keep all of the objects with the same anchor points.
-					child.anchorMin = child.anchorMax = child.pivot = new Vector2(0.5f, 0.5f);
-					fAngle += fOffsetAngle;
+
+					if (m_useZIncrement)
+					{
+						child.localPosition += new Vector3(0,0, z);
+						z += m_zIncrement;
+					}
+
+					if (m_rotateElements)
+					{
+						child.localRotation = Quaternion.AngleAxis(angle + m_rotationAngleOffset, Vector3.forward);
+					}
+					angle += offsetAngle;
 				}
 			}
 		}
+
+		private DrivenTransformProperties GetDrivenTransformProperties()
+		{
+			DrivenTransformProperties result =
+				  DrivenTransformProperties.Anchors
+				| DrivenTransformProperties.AnchoredPosition
+				| DrivenTransformProperties.Pivot;
+
+			if (m_useZIncrement)
+				result |= DrivenTransformProperties.AnchoredPositionZ;
+
+			if (m_rotateElements)
+				result |= DrivenTransformProperties.Rotation;
+
+			return result;
+		}
 	}
+
+#if UNITY_EDITOR
+	[CustomEditor(typeof(UiRadialLayoutGroup))]
+	public class UiRadialLayoutGroupEditor : Editor
+	{
+		protected SerializedProperty m_radiusProp;
+		protected SerializedProperty m_zIncrementProp;
+		protected SerializedProperty m_rotateElementsProp;
+		protected SerializedProperty m_rotationAngleOffsetProp;
+
+		static private bool m_toolsVisible;
+
+		public virtual void OnEnable()
+		{
+			m_radiusProp = serializedObject.FindProperty("m_radius");
+			m_zIncrementProp = serializedObject.FindProperty("m_zIncrement");
+			m_rotateElementsProp = serializedObject.FindProperty("m_rotateElements");
+			m_rotationAngleOffsetProp = serializedObject.FindProperty("m_rotationAngleOffset");
+		}
+
+		public override void OnInspectorGUI()
+		{
+			UiRadialLayoutGroup thisUiRadialLayoutGroup = (UiRadialLayoutGroup)target;
+
+			EditorGUILayout.PropertyField(m_radiusProp);
+			EditorGUILayout.PropertyField(m_zIncrementProp);
+			EditorGUILayout.PropertyField(m_rotateElementsProp);
+			EditorGUILayout.PropertyField(m_rotationAngleOffsetProp);
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+	}
+#endif
+
 }
