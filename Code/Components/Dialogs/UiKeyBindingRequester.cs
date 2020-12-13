150f90ea4f1cd1a82bc5aa36805533658c153713
phoenix <phoenix@phoenixgrafik.de>
2020-12-13 01:51:40 +0100

    Translations for key bindings requester

--- Code/Components/Dialogs/UiKeyBindingRequester.cs
+++ Code/Components/Dialogs/UiKeyBindingRequester.cs
@@ -19,21 +19,24 @@ namespace GuiToolkit
 			m_restoreDefaultsButton.OnClick.AddListener(OnRestoreDefaultButton);
 		}
 
-		public void Requester( string _title = "Key Bindings", bool _allowOutsideTap = false, UnityAction _onOk = null, UnityAction _onCancel = null, string _okText = null, string _cancelText = null )
+		public void Requester( string _title = null, bool _allowOutsideTap = false, UnityAction _onOk = null, UnityAction _onCancel = null, string _okText = null, string _cancelText = null )
 		{
 			UiMain.Instance.KeyBindings.BeginChangeBindings();
+
+			if (_title == null)
+				_title = _("Key Bindings");
 
 			Options options = new Options
 			{
 				ButtonInfos = new ButtonInfo[] 
 				{
 					new ButtonInfo {
-						Text = string.IsNullOrEmpty(_okText) ? "OK" : _okText,
+						Text = string.IsNullOrEmpty(_okText) ? _("Ok") : _okText,
 						Prefab = m_okButtonPrefab,
 						OnClick = () => { OnOk( _onOk ); }
 					},
 					new ButtonInfo {
-						Text = string.IsNullOrEmpty(_cancelText) ? "Cancel" : _cancelText,
+						Text = string.IsNullOrEmpty(_cancelText) ? _("Cancel") : _cancelText,
 						Prefab = m_cancelButtonPrefab,
 						OnClick = () => { OnCancel( _onCancel ); }
 					}
