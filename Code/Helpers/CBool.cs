a7f4b1b2105eb8dd984f01f0fb1fb9a52ef9c547
phoenix <phoenix@phoenixgrafik.de>
2020-12-13 21:42:40 +0100

    CBool now in a separate file

---
+++ Code/Helpers/CBool.cs
@@ -0,0 +1,20 @@
+﻿namespace GuiToolkit
+{
+	// A struct mimicking C "bool" handling
+	// As C doesn't know bools but only ints, you can do all kinds of numerical and logical operations
+	// on them. C# is different; it strictly distinguishes between bool (only logical operations) and int (only numerical operations)
+	// CBool mimicks this behaviour by implicit conversions, so that you can
+	// write CBool cb = 100 as well as CBool cb = true.
+	// As it contradicts the usual C# rules and is questionable regarding performance, it should be only used for special cases like integrating 
+	// C code without changes (See LocaPlurals.cs)
+	public struct CBool
+	{
+		private int m_value;
+		public static implicit operator bool(CBool _val) => _val.m_value != 0;
+		public static implicit operator int(CBool _val) => _val.m_value;
+		public static implicit operator CBool(int _val) => new CBool(_val);
+		public static implicit operator CBool(bool _val) => new CBool(_val);
+		public CBool(int _val = 0) { m_value = _val; }
+		public CBool(bool _val) { m_value = _val ? 1 : 0; }
+	}
+}
--- Code/Loca/LocaPlurals.cs
+++ Code/Loca/LocaPlurals.cs
@@ -4,18 +4,6 @@ namespace GuiToolkit
 {
 	public static class LocaPlurals
 	{
-		// Mimicking C bool handling
-		public struct CBool
-		{
-			private int m_value;
-			public static implicit operator bool(CBool _val) => _val.m_value != 0;
-			public static implicit operator int(CBool _val) => _val.m_value;
-			public static implicit operator CBool(int _val) => new CBool(_val);
-			public static implicit operator CBool(bool _val) => new CBool(_val);
-			public CBool(int _val = 0) { m_value = _val; }
-			public CBool(bool _val) { m_value = _val ? 1 : 0; }
-		}
-		
 		public static (int numPluralForms, int pluralIdx) GetPluralIdx(string _languageId, int _number)
 		{
 			int nplurals = 0, n = _number;
