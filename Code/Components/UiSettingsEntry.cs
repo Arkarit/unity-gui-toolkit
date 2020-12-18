a8c781d523c17acddf01fdade2cbec3a400460c0
phoenix <phoenix@phoenixgrafik.de>
2020-12-18 23:37:50 +0100

    Cont implementing full screen settings dialog

---
+++ Code/Components/UiSettingsEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace GuiToolkit
+{
+	public class UiSettingsEntry : UiThing
+	{
+		[SerializeField]
+		protected TMP_Text m_text;
+
+		[SerializeField]
+		protected UiToggle m_toggle;
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			m_toggle.OnValueChanged.AddListener(OnValueChanged);
+		}
+
+		private void OnValueChanged( bool _val )
+		{
+			Debug.Log($"TODO: Evaluate toggle '{gameObject.name}': {_val}");
+		}
+
+		protected override void OnDisable()
+		{
+			m_toggle.OnValueChanged.RemoveListener(OnValueChanged);
+			base.OnDisable();
+		}
+	}
+}
