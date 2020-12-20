b82a4c76c48409fbf0b8de843b819ff24b0db20d
phoenix <phoenix@phoenixgrafik.de>
2020-12-20 22:51:13 +0100

    Splitter now also handles mouse clicks on slider

--- Code/Components/UiHorizontalVerticalSplitter.cs
+++ Code/Components/UiHorizontalVerticalSplitter.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System;
+using System.Collections;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,8 +11,10 @@ using UnityEditor;
 
 namespace GuiToolkit
 {
-	public class UiHorizontalVerticalSplitter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+	public class UiHorizontalVerticalSplitter : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler //, IPointerClickHandler //, IPointerUpHandler
 	{
+		private const float DRAG_DETECTION_TIME = 0.2f;
+
 		public Slider m_slider;
 		public ScrollRect m_scrollRect;
 
@@ -20,18 +23,23 @@ namespace GuiToolkit
 		private IBeginDragHandler m_beginDragHandler;
 		private IDragHandler m_dragHandler;
 		private IEndDragHandler m_endDragHandler;
+		private IPointerDownHandler m_pointerDownHandler;
 		private bool m_handlersDetermined;
+		private bool m_wasDragged;
 
 		public void OnBeginDrag( PointerEventData _eventData )
 		{
 			Debug.Log("OnBeginDrag");
 			m_eventData = _eventData.Clone();
 			m_handlersDetermined = false;
+			m_wasDragged = true;
 		}
 
 		public void OnDrag( PointerEventData _eventData )
 		{
-			InitHorizontal(_eventData);
+			if (m_eventData != null)
+				m_isHorizontal = EvalHorizontal(m_eventData, _eventData);
+
 			if (m_isHorizontal == ETriState.Indeterminate)
 				return;
 
@@ -39,15 +47,7 @@ namespace GuiToolkit
 			{
 				m_handlersDetermined = true;
 				MonoBehaviour mb = (m_isHorizontal == ETriState.True) ? (MonoBehaviour) m_slider : (MonoBehaviour) m_scrollRect;
-
-				if (mb is IBeginDragHandler)
-					m_beginDragHandler = (IBeginDragHandler) mb;
-
-				if (mb is IDragHandler)
-					m_dragHandler = (IDragHandler) mb;
-
-				if (mb is IEndDragHandler)
-					m_endDragHandler = (IEndDragHandler) mb;
+				SetHandlers(mb);
 			}
 
 			Debug.Log("OnDrag");
@@ -79,6 +79,7 @@ namespace GuiToolkit
 		public void OnPointerDown( PointerEventData _eventData )
 		{
 			Debug.Log("OnPointerDown");
+			StartCoroutine(DelayedOnPointerDown(_eventData.Clone()));
 		}
 
 		public void OnPointerUp( PointerEventData _eventData )
@@ -86,27 +87,32 @@ namespace GuiToolkit
 			Debug.Log("OnPointerUp");
 		}
 
-		private void InitHorizontal( PointerEventData _eventData )
+		private void SetHandlers( MonoBehaviour _monoBehaviour )
 		{
-			if (m_eventData != null)
-			{
-				m_isHorizontal = EvalHorizontal(m_eventData, _eventData);
-
-				if (m_isHorizontal == ETriState.True && m_slider is IBeginDragHandler)
-					((IBeginDragHandler)m_slider).OnBeginDrag(m_eventData);
-			}
+			m_beginDragHandler = _monoBehaviour is IBeginDragHandler ? (IBeginDragHandler)_monoBehaviour : null;
+			m_dragHandler = _monoBehaviour is IDragHandler ? (IDragHandler)_monoBehaviour : null;
+			m_endDragHandler = _monoBehaviour is IEndDragHandler ? (IEndDragHandler)_monoBehaviour : null;
+			m_pointerDownHandler = _monoBehaviour is IPointerDownHandler ? (IPointerDownHandler)_monoBehaviour : null;
 		}
 
 		private static ETriState EvalHorizontal( PointerEventData _before, PointerEventData _after )
 		{
 			Vector2 delta = _after.position - _before.position;
 			Vector2 deltaAbs = new Vector2(Mathf.Abs(delta.x), Mathf.Abs(delta.y));
-Debug.Log($"_before.position:{_before.position} _after.position:{_after.position} deltaAbs:{deltaAbs}");
 			if (deltaAbs.magnitude < 1)
 				return ETriState.Indeterminate;
 
 			Debug.Log($"delta: {delta}");
 			return deltaAbs.x > deltaAbs.y ? ETriState.True : ETriState.False;
+		}
+
+		private IEnumerator DelayedOnPointerDown(PointerEventData _eventData)
+		{
+			yield return new WaitForSecondsRealtime(DRAG_DETECTION_TIME);
+
+			if (!m_wasDragged)
+				m_slider.OnPointerDown(_eventData);
+			m_wasDragged = false;
 		}
 
 
