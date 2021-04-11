41de9e5b7969946c76accf20fbb076397b10b032
phoenix <phoenix@phoenixgrafik.de>
2021-04-11 09:26:33 +0200

    Resolution dependent switcher for dynamic landscape/portrait change WIP

--- Code/Components/UiResolutionDependentSwitcher.cs
+++ Code/Components/UiResolutionDependentSwitcher.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace GuiToolkit
 {
+	[Serializable]
+	public struct ResolutionDependentDefinition
+	{
+		public Component Target;
+		public Component TemplateLandscape;
+		public Component TemplatePortrait;
+	}
+
 	[RequireComponent(typeof(RectTransform))]
 	[ExecuteAlways]
 	public class UiResolutionDependentSwitcher : MonoBehaviour
 	{
-		[Header("Landscape")]
-		[SerializeField] protected UiHorizontalOrVerticalLayoutGroup[] m_horizontalInLandscape = new UiHorizontalOrVerticalLayoutGroup[0];
+		[SerializeField] protected ResolutionDependentDefinition[] m_definitions = new ResolutionDependentDefinition[0];
 
-		[Header("Portrait")]
-		[SerializeField] protected UiHorizontalOrVerticalLayoutGroup[] m_verticalInLandscape = new UiHorizontalOrVerticalLayoutGroup[0];
+		public ResolutionDependentDefinition[] Definitions => m_definitions;
 
 		protected virtual void Start()
 		{
@@ -29,14 +39,56 @@ namespace GuiToolkit
 		{
 			bool isLandscape = Screen.width >= Screen.height;
 
-			foreach( var layout in m_horizontalInLandscape )
-				if (layout != null)
-					layout.Vertical = !isLandscape;
+			foreach( var definition in m_definitions )
+			{
+				definition.Target.CopyFrom(isLandscape ? definition.TemplateLandscape : definition.TemplatePortrait);
+			}
+		}
+	}
 
-			foreach( var layout in m_verticalInLandscape )
-				if (layout != null)
-					layout.Vertical = isLandscape;
+#if UNITY_EDITOR
+	/// \addtogroup Editor Code
+	/// UiMaterialCloner is quite fragile regarding its options and thus needs a special
+	/// treatment in the editor
+	[CustomEditor(typeof(UiResolutionDependentSwitcher))]
+	public class UiResolutionDependentSwitcherEditor : Editor
+	{
+		protected SerializedProperty m_definitionsProperty;
+
+		private void OnEnable()
+		{
+			m_definitionsProperty = serializedObject.FindProperty("m_definitions");
 		}
 
+		public override void OnInspectorGUI()
+		{
+			var thisUiResolutionDependentSwitcher = (UiResolutionDependentSwitcher) target;
+
+			EditorGUILayout.PropertyField(m_definitionsProperty, true);
+
+			serializedObject.ApplyModifiedProperties();
+
+			GUILayout.Space(UiEditorUtility.LARGE_SPACE_HEIGHT);
+
+			if (GUILayout.Button("Apply"))
+			{
+				bool isLandscape = Screen.width >= Screen.height;
+
+				Undo.SetCurrentGroupName("Apply Resolution dependent components");
+				foreach (var definition in thisUiResolutionDependentSwitcher.Definitions)
+				{
+					Component target = isLandscape ? definition.TemplateLandscape : definition.TemplatePortrait;
+					Component source = definition.Target;
+					if (source == null || target == null)
+						continue;
+
+					Undo.RegisterCompleteObjectUndo(target, "");
+					target.CopyFrom(source);
+				}
+				Undo.CollapseUndoOperations(Undo.GetCurrentGroup());
+			}
+		}
 	}
+#endif
+
 }
