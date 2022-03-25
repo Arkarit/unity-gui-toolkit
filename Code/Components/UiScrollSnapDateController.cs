20a0a74df108a0eb3c01b4e5821c261fd7997cb6
phoenix <phoenix@phoenixgrafik.de>
2022-03-25 22:23:30 +0100

    UiScrollSnapDateController heavy WIP

---
+++ Code/Components/UiScrollSnapDateController.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+namespace GuiToolkit
+{
+	public class UiScrollSnapDateController : UiThing
+	{
+		[SerializeField] protected UiScrollSnapText m_day;
+		[SerializeField] protected UiScrollSnapText m_month;
+		[SerializeField] protected UiScrollSnapText m_year;
+
+		public DateTime Date
+		{
+			get => GetDate();
+			set => SetDate(value);
+		}
+
+		protected override void Start()
+		{
+			base.Start();
+			SetDate(DateTime.Now);
+		}
+
+		private DateTime GetDate()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void SetDate( DateTime _date )
+		{
+//			m_day.SetNumbers(1, DateTime.DaysInMonth(_date.Year, _date.Month));
+//			m_day.SetCurrentPage(_date.Day);
+		}
+	}
+}
