dace4f9ed462337690804e38e240aa1c97f8fb58
phoenix <phoenix@phoenixgrafik.de>
2022-03-26 13:26:52 +0100

    Cont changing UiScrollRect to support snapping

--- Code/Components/UiScrollRect.cs
+++ Code/Components/UiScrollRect.cs
@@ -10,7 +10,7 @@ namespace GuiToolkit
 	public class UiScrollRect : UiThing
 	{
 		[SerializeField] protected bool m_useInitialNormalizedPosition;
-		[SerializeField] protected Vector2 m_initialNormalizedPosition = new Vector2(0,1);
+		[SerializeField] protected Vector2 m_initialNormalizedPosition = new Vector2(0, 1);
 
 		[Tooltip("Duration of tween to make child visible. 0 means instant.")]
 		[SerializeField] protected float m_ensureChildVisibilityDuration = 0.3f;
@@ -18,10 +18,17 @@ namespace GuiToolkit
 		[Tooltip("Padding (Fraction of child width). If a child isn't the first or the last child, it makes sense to also display the neighbor tabs. This distance is used for that.")]
 		[SerializeField] protected float m_ensureChildVisibilityPadding = 0.4f;
 
+		[Tooltip("Snapping enabled or disabled")]
+		[SerializeField] protected bool m_snappingEnabled;
+
+		[Tooltip("Scroll rect velocity, below which it is snapped")]
+		[SerializeField] protected float m_snapBelowSpeed = 20.0f;
+
 		private ScrollRect m_scrollRect;
 		private Vector3Tween m_ensureChildVisibilityTween = null;
 		private RectTransform m_content;
 		private RectTransform m_viewport;
+		private bool m_moving;
 
 		#region Getters
 		protected bool IsEnsureChildVisibilityAnimated => !Mathf.Approximately(m_ensureChildVisibilityDuration, 0);
@@ -58,7 +65,7 @@ namespace GuiToolkit
 		#endregion
 
 		#region Add, remove and find Items
-		public virtual void AddItem(RectTransform _item, int _idx = -1)
+		public virtual void AddItem( RectTransform _item, int _idx = -1 )
 		{
 			if (_item.parent == Content)
 				throw new ArgumentException($"Can not add '{_item.gameObject.name}' to UiScrollRect '{gameObject.name}': Item was already added");
@@ -68,17 +75,17 @@ namespace GuiToolkit
 				_item.SetSiblingIndex(_idx);
 		}
 
-		public virtual void AddPooledItem(GameObject _prefab, int _idx = -1)
+		public virtual void AddPooledItem( GameObject _prefab, int _idx = -1 )
 		{
-			AddPooledItem((RectTransform) _prefab.transform);
+			AddPooledItem((RectTransform)_prefab.transform);
 		}
 
-		public virtual void AddPooledItem(RectTransform _prefabRt, int _idx = -1)
+		public virtual void AddPooledItem( RectTransform _prefabRt, int _idx = -1 )
 		{
 			RectTransform rt = _prefabRt.PoolInstantiate();
 		}
 
-		public virtual void RemoveItem(RectTransform _item, bool _destroy = true)
+		public virtual void RemoveItem( RectTransform _item, bool _destroy = true )
 		{
 			if (_item.parent != Content)
 				throw new ArgumentException($"Can not remove '{_item.gameObject.name}' from UiScrollRect '{gameObject.name}': Item not child of the scroll rect");
@@ -89,12 +96,40 @@ namespace GuiToolkit
 				_item.PoolDestroy();
 		}
 
-		public virtual void RemoveItem(int _idx, bool _destroy = false) => RemoveItem(GetContentChild(_idx), _destroy);
+		public virtual void RemoveItem( int _idx, bool _destroy = false ) => RemoveItem(GetContentChild(_idx), _destroy);
 
-		public virtual void RemoveAllItems(bool _destroy = true)
+		public virtual void RemoveAllItems( bool _destroy = true )
 		{
 			while (Content.childCount > 0)
 				RemoveItem(0, _destroy);
+		}
+
+		RectTransform GetItemNextToCenter()
+		{
+			float minDistance = 99999999.0f;
+			RectTransform result = null;
+
+			Rect viewportRect = Viewport.GetScreenRect();
+			Vector2 viewportCenter = viewportRect.center;
+
+			bool found = false;
+			foreach (RectTransform child in Content)
+			{
+				Rect childRect = child.GetScreenRect();
+				float distance = Vector2.Distance(viewportCenter, childRect.center);
+				if (distance < minDistance)
+				{
+					minDistance = distance;
+					result = child;
+					found = true;
+					continue;
+				}
+
+				if (found)
+					break;
+			}
+
+			return result;
 		}
 		#endregion
 
@@ -154,12 +189,18 @@ namespace GuiToolkit
 				Content.position = t.CurrentValue;
 			};
 
-			m_ensureChildVisibilityTween = gameObject.Tween("makeVisible", startPos, pos, m_ensureChildVisibilityDuration, TweenScaleFunctions.QuadraticEaseInOut, updateBar);
+			System.Action<ITween<Vector3>> stopMoving = ( t ) =>
+			{
+				m_moving = false;
+			};
+
+			m_moving = true;
+			m_ensureChildVisibilityTween = gameObject.Tween("makeVisible", startPos, pos, m_ensureChildVisibilityDuration, TweenScaleFunctions.QuadraticEaseInOut, updateBar, stopMoving);
 		}
 
 		#endregion
 
-		private float GetScrollValue(bool _centered, float _xyViewport, float _whViewport, float _xyChild, float _whChild)
+		private float GetScrollValue( bool _centered, float _xyViewport, float _whViewport, float _xyChild, float _whChild )
 		{
 			if (_centered)
 				return _xyViewport - _xyChild + (_whViewport - _whChild) / 2;
@@ -173,13 +214,40 @@ namespace GuiToolkit
 			return 0;
 		}
 
-		protected RectTransform GetContentChild(int _idx) => (RectTransform) Content.GetChild(_idx);
+		protected RectTransform GetContentChild( int _idx ) => (RectTransform)Content.GetChild(_idx);
 
 		protected override void OnEnable()
 		{
 			base.OnEnable();
 			if (m_useInitialNormalizedPosition)
 				StartCoroutine(DelayedScrollToTop());
+		}
+
+		protected virtual void Update()
+		{
+			Debug.Log($"velocity:{ScrollRect.velocity} mouse:{Input.GetMouseButton(0)}");
+
+			if (!m_snappingEnabled || m_moving || Input.GetMouseButton(0))
+				return;
+
+			Vector2 vel2 = ScrollRect.velocity;
+			float velocity = ScrollRect.horizontal ? vel2.x : vel2.y;
+
+			if (Mathf.Approximately(0, velocity))
+				return;
+
+			if (velocity < m_snapBelowSpeed)
+			{
+				Snap();
+			}
+		}
+
+		private void Snap()
+		{
+			ScrollRect.velocity = Vector2.zero;
+			RectTransform item = GetItemNextToCenter();
+			if (item)
+				EnsureVisible(item, true);
 		}
 
 		protected IEnumerator DelayedScrollToTop()
