79971c8ab87c88a6cdd2bbf9615b4f8104457dc0
Arkarit <phoenix@phoenixgrafik.de>
2024-05-03 00:12:20 +0200

    Clean up and removed experimental object from scene

--- Runtime/Code/TestAndExperiments/TestPrefabChanges.cs
+++ Runtime/Code/TestAndExperiments/TestPrefabChanges.cs
@@ -6,6 +6,7 @@ using UnityEngine.Rendering;
 public class TestPrefabChanges : MonoBehaviour
 {
 	public bool m_running;
+	public bool m_useRevertOverridesScope = true;
 
 	void Update()
 	{
@@ -15,7 +16,7 @@ public class TestPrefabChanges : MonoBehaviour
 		var renderers = GetComponentsInChildren<MeshRenderer>(true);
 		foreach (var rend in renderers)
 		{
-			using (new RevertOverridesScope(rend))
+			using (new RevertOverridesScope(m_useRevertOverridesScope ? rend : null))
 			{
 				rend.enabled = !rend.enabled;
 				rend.shadowCastingMode = rend.shadowCastingMode == ShadowCastingMode.Off ? 
