1ac7c7995905821c71c2cf742d72d313633b6b70
Arkarit <phoenix@phoenixgrafik.de>
2024-05-20 12:51:00 +0200

    Fixed styles applicableness was not synchronized

--- Runtime/Code/StylingSystem/UiStyleUtility.cs
+++ Runtime/Code/StylingSystem/UiStyleUtility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
@@ -6,29 +7,26 @@ namespace GuiToolkit.Style
 {
 	public static class UiStyleUtility
 	{
-		public static string GetName(Type _supportedMonoBehaviourType, string _name)
+		public static string GetName( Type _supportedMonoBehaviourType, string _name )
 		{
 			return $"{_supportedMonoBehaviourType.Name} Style: {_name}";
 		}
 
-		public static int GetKey(Type _supportedMonoBehaviourType, string _name)
+		public static int GetKey( Type _supportedMonoBehaviourType, string _name )
 		{
 			return Animator.StringToHash(GetName(_supportedMonoBehaviourType, _name));
 		}
 
-		public static void SynchronizeApplicableness(UiAbstractStyleBase _from, UiAbstractStyleBase _to)
+		public static void SynchronizeApplicableness( UiAbstractStyleBase _from, UiAbstractStyleBase _to )
 		{
-			if (   _from == null 
-			    || _to == null 
-			    || _from.GetType() != _to.GetType())
+			if (_from == null
+				|| _to == null
+				|| _from.GetType() != _to.GetType())
 				return;
 
 			var members = _from.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
 			foreach (var member in members)
 			{
-				if (member.FieldType.BaseType != typeof(ApplicableValueBase))
-					continue;
-
 				ApplicableValueBase fromVal = member.GetValue(_from) as ApplicableValueBase;
 				ApplicableValueBase toVal = member.GetValue(_to) as ApplicableValueBase;
 				if (fromVal == null || toVal == null)
