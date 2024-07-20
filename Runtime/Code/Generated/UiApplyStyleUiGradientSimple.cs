7e97804799ad617a0c128c9436778fc67ead19b9
Arkarit <phoenix@phoenixgrafik.de>
2024-07-20 11:09:34 +0200

    Styling system WIP

---
+++ Runtime/Code/Generated/UiApplyStyleUiGradientSimple.cs
@@ -0,0 +1,52 @@
+// Auto-generated, please do not change!
+using UnityEngine;
+using GuiToolkit.Style;
+
+namespace GuiToolkit.Style
+{	[ExecuteAlways]
+	public class UiApplyStyleUiGradientSimple : UiAbstractApplyStyle<GuiToolkit.UiGradientSimple, UiStyleUiGradientSimple>
+	{
+		public override void Apply()
+		{
+			if (!SpecificMonoBehaviour || SpecificStyle == null)
+				return;
+
+			if (SpecificStyle.ColorLeftOrTop.IsApplicable)
+				try { SpecificMonoBehaviour.ColorLeftOrTop = SpecificStyle.ColorLeftOrTop.Value; } catch {}
+			if (SpecificStyle.ColorRightOrBottom.IsApplicable)
+				try { SpecificMonoBehaviour.ColorRightOrBottom = SpecificStyle.ColorRightOrBottom.Value; } catch {}
+			if (SpecificStyle.Axis.IsApplicable)
+				try { SpecificMonoBehaviour.Axis = SpecificStyle.Axis.Value; } catch {}
+		}
+
+		public override UiAbstractStyleBase CreateStyle(string _name, UiAbstractStyleBase _template = null)
+		{
+			UiStyleUiGradientSimple result = new UiStyleUiGradientSimple();
+
+			if (!SpecificMonoBehaviour)
+				return result;
+
+			result.Name = _name;
+
+			if (_template != null)
+			{
+				var specificTemplate = (UiStyleUiGradientSimple) _template;
+
+				result.ColorLeftOrTop.Value = specificTemplate.ColorLeftOrTop.Value;
+				result.ColorLeftOrTop.IsApplicable = specificTemplate.ColorLeftOrTop.IsApplicable;
+				result.ColorRightOrBottom.Value = specificTemplate.ColorRightOrBottom.Value;
+				result.ColorRightOrBottom.IsApplicable = specificTemplate.ColorRightOrBottom.IsApplicable;
+				result.Axis.Value = specificTemplate.Axis.Value;
+				result.Axis.IsApplicable = specificTemplate.Axis.IsApplicable;
+
+				return result;
+			}
+
+			try { result.ColorLeftOrTop.Value = SpecificMonoBehaviour.ColorLeftOrTop; } catch {}
+			try { result.ColorRightOrBottom.Value = SpecificMonoBehaviour.ColorRightOrBottom; } catch {}
+			try { result.Axis.Value = SpecificMonoBehaviour.Axis; } catch {}
+
+			return result;
+		}
+	}
+}
---
+++ Runtime/Code/Generated/UiStyleUiGradientSimple.cs
@@ -0,0 +1,22 @@
+// Auto-generated, please do not change!
+using System;
+using UnityEngine;
+using GuiToolkit.Style;
+
+namespace GuiToolkit.Style
+{
+	[Serializable]
+	public class UiStyleUiGradientSimple : UiAbstractStyle<GuiToolkit.UiGradientSimple>
+	{
+		private class ApplicableValueColor : ApplicableValue<UnityEngine.Color> {}
+		private class ApplicableValueEAxis2D : ApplicableValue<GuiToolkit.EAxis2D> {}
+
+		[SerializeReference] private ApplicableValueColor m_ColorLeftOrTop = new();
+		[SerializeReference] private ApplicableValueColor m_ColorRightOrBottom = new();
+		[SerializeReference] private ApplicableValueEAxis2D m_Axis = new();
+
+		public ApplicableValue<UnityEngine.Color> ColorLeftOrTop => m_ColorLeftOrTop;
+		public ApplicableValue<UnityEngine.Color> ColorRightOrBottom => m_ColorRightOrBottom;
+		public ApplicableValue<GuiToolkit.EAxis2D> Axis => m_Axis;
+	}
+}
