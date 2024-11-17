7bc0587726b59018c29e7ffa1fb1f9a342636a87
Arkarit <phoenix@phoenixgrafik.de>
2024-11-17 23:46:05 +0100

    Icons now without baked shadow

---
+++ Runtime/Code/Generated/UiApplyStyleShadow.cs
@@ -0,0 +1,73 @@
+// Auto-generated, please do not change!
+using UnityEngine;
+using GuiToolkit;
+using GuiToolkit.Style;
+
+namespace GuiToolkit.Style
+{
+	[ExecuteAlways]
+	[RequireComponent(typeof(UnityEngine.UI.Shadow))]
+	public class UiApplyStyleShadow : UiAbstractApplyStyle<UnityEngine.UI.Shadow, UiStyleShadow>
+	{
+		protected override void ApplyImpl()
+		{
+			if (!SpecificComponent || SpecificStyle == null)
+				return;
+
+			if (SpecificStyle.EffectColor.IsApplicable)
+				try { SpecificComponent.effectColor = Tweenable ? SpecificStyle.EffectColor.Value : SpecificStyle.EffectColor.RawValue; } catch {}
+			if (SpecificStyle.EffectDistance.IsApplicable)
+				try { SpecificComponent.effectDistance = Tweenable ? SpecificStyle.EffectDistance.Value : SpecificStyle.EffectDistance.RawValue; } catch {}
+			if (SpecificStyle.Enabled.IsApplicable)
+				try { SpecificComponent.enabled = Tweenable ? SpecificStyle.Enabled.Value : SpecificStyle.Enabled.RawValue; } catch {}
+			if (SpecificStyle.UseGraphicAlpha.IsApplicable)
+				try { SpecificComponent.useGraphicAlpha = Tweenable ? SpecificStyle.UseGraphicAlpha.Value : SpecificStyle.UseGraphicAlpha.RawValue; } catch {}
+		}
+
+		protected override void RecordImpl()
+		{
+			if (!SpecificComponent || SpecificStyle == null)
+				return;
+
+			if (SpecificStyle.EffectColor.IsApplicable)
+				try { SpecificStyle.EffectColor.RawValue = SpecificComponent.effectColor; } catch {}
+			if (SpecificStyle.EffectDistance.IsApplicable)
+				try { SpecificStyle.EffectDistance.RawValue = SpecificComponent.effectDistance; } catch {}
+			if (SpecificStyle.Enabled.IsApplicable)
+				try { SpecificStyle.Enabled.RawValue = SpecificComponent.enabled; } catch {}
+			if (SpecificStyle.UseGraphicAlpha.IsApplicable)
+				try { SpecificStyle.UseGraphicAlpha.RawValue = SpecificComponent.useGraphicAlpha; } catch {}
+		}
+
+		public override UiAbstractStyleBase CreateStyle(UiStyleConfig _styleConfig, string _name, UiAbstractStyleBase _template = null)
+		{
+			UiStyleShadow result = new UiStyleShadow(_styleConfig, _name);
+
+			if (!SpecificComponent)
+				return result;
+
+			if (_template != null)
+			{
+				var specificTemplate = (UiStyleShadow) _template;
+
+				result.EffectColor.Value = specificTemplate.EffectColor.Value;
+				result.EffectColor.IsApplicable = specificTemplate.EffectColor.IsApplicable;
+				result.EffectDistance.Value = specificTemplate.EffectDistance.Value;
+				result.EffectDistance.IsApplicable = specificTemplate.EffectDistance.IsApplicable;
+				result.Enabled.Value = specificTemplate.Enabled.Value;
+				result.Enabled.IsApplicable = specificTemplate.Enabled.IsApplicable;
+				result.UseGraphicAlpha.Value = specificTemplate.UseGraphicAlpha.Value;
+				result.UseGraphicAlpha.IsApplicable = specificTemplate.UseGraphicAlpha.IsApplicable;
+
+				return result;
+			}
+
+			try { result.EffectColor.Value = SpecificComponent.effectColor; } catch {}
+			try { result.EffectDistance.Value = SpecificComponent.effectDistance; } catch {}
+			try { result.Enabled.Value = SpecificComponent.enabled; } catch {}
+			try { result.UseGraphicAlpha.Value = SpecificComponent.useGraphicAlpha; } catch {}
+
+			return result;
+		}
+	}
+}
---
+++ Runtime/Code/Generated/UiStyleShadow.cs
@@ -0,0 +1,79 @@
+// Auto-generated, please do not change!
+using System;
+using UnityEngine;
+using GuiToolkit;
+using GuiToolkit.Style;
+
+namespace GuiToolkit.Style
+{
+	[Serializable]
+	public class UiStyleShadow : UiAbstractStyle<UnityEngine.UI.Shadow>
+	{
+		public UiStyleShadow(UiStyleConfig _styleConfig, string _name)
+		{
+			StyleConfig = _styleConfig;
+			Name = _name;
+		}
+
+		private class ApplicableValueColor : ApplicableValue<UnityEngine.Color> {}
+		private class ApplicableValueVector2 : ApplicableValue<UnityEngine.Vector2> {}
+		private class ApplicableValueBoolean : ApplicableValue<System.Boolean> {}
+
+		protected override ApplicableValueBase[] GetValueList()
+		{
+			return new ApplicableValueBase[]
+			{
+				EffectColor,
+				EffectDistance,
+				Enabled,
+				UseGraphicAlpha,
+			};
+		}
+
+		[SerializeReference] private ApplicableValueColor m_effectColor = new();
+		[SerializeReference] private ApplicableValueVector2 m_effectDistance = new();
+		[SerializeReference] private ApplicableValueBoolean m_enabled = new();
+		[SerializeReference] private ApplicableValueBoolean m_useGraphicAlpha = new();
+
+		public ApplicableValue<UnityEngine.Color> EffectColor
+		{
+			get
+			{
+				if (m_effectColor == null)
+					m_effectColor = new ApplicableValueColor();
+				return m_effectColor;
+			}
+		}
+
+		public ApplicableValue<UnityEngine.Vector2> EffectDistance
+		{
+			get
+			{
+				if (m_effectDistance == null)
+					m_effectDistance = new ApplicableValueVector2();
+				return m_effectDistance;
+			}
+		}
+
+		public ApplicableValue<System.Boolean> Enabled
+		{
+			get
+			{
+				if (m_enabled == null)
+					m_enabled = new ApplicableValueBoolean();
+				return m_enabled;
+			}
+		}
+
+		public ApplicableValue<System.Boolean> UseGraphicAlpha
+		{
+			get
+			{
+				if (m_useGraphicAlpha == null)
+					m_useGraphicAlpha = new ApplicableValueBoolean();
+				return m_useGraphicAlpha;
+			}
+		}
+
+	}
+}
