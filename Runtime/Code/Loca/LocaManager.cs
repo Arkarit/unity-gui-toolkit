abfda22c5a8bf392d0d0a75aca52b3564a35b92a
Arkarit <phoenix@phoenixgrafik.de>
2024-12-27 09:04:34 +0100

    Setting culture in LocaManager

--- Runtime/Code/Loca/LocaManager.cs
+++ Runtime/Code/Loca/LocaManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 using UnityEngine;
 
 namespace GuiToolkit
@@ -64,6 +66,8 @@ namespace GuiToolkit
 			Language = _languageId;
 			PlayerPrefs.SetString(PLAYER_PREFS_KEY, Language);
 
+			SetCulture(_languageId);
+
 			if (!ChangeLanguageImpl(_languageId))
 			{
 				Debug.LogWarning($"Language '{_languageId}' not found");
@@ -75,6 +79,31 @@ namespace GuiToolkit
 				UiEventDefinitions.EvLanguageChanged.Invoke(_languageId);
 
 			return true;
+		}
+
+		private static void SetCulture(string _languageId)
+		{
+			try
+			{
+				CultureInfo ci = new CultureInfo(_languageId);
+				Thread.CurrentThread.CurrentCulture = ci;
+				Thread.CurrentThread.CurrentUICulture = ci;
+			}
+			catch (CultureNotFoundException _)
+			{
+				Debug.LogWarning($"Culture '{_languageId}' not found, setting default culture");
+
+				try
+				{
+					CultureInfo ci = new CultureInfo("en");
+					Thread.CurrentThread.CurrentCulture = ci;
+					Thread.CurrentThread.CurrentUICulture = ci;
+				}
+				catch (CultureNotFoundException __)
+				{
+					Debug.LogError("Could not set default culture 'en'");
+				}
+			}
 		}
 	}
 }
