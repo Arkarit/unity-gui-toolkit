ae950cc59961dd0e17af69521df0d9dcc802b80c
Arkarit <phoenix@phoenixgrafik.de>
2024-12-28 09:51:40 +0100

    Grid picker WIP

--- Runtime/Code/DateTimePicker/HourOptionData.cs
+++ Runtime/Code/DateTimePicker/HourOptionData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,9 @@ namespace GuiToolkit
 		[SerializeField] private bool m_is24hour;
 		[SerializeField] private UiButton m_button;
 		[SerializeField] private UiGridPickerCell m_gridPickerCellPrefab;
+		[SerializeField] private int m_numColumns = 6;
+		[SerializeField] private int m_numRows = 4;
+		[SerializeField] private int m_maxElements = 24;
 
 		public int SelectedValue { get; internal set; }
 
@@ -28,14 +32,14 @@ namespace GuiToolkit
 		{
 			var options = new UiGridPicker.Options()
 			{
-				NumColumns = 6,
-				NumRows = 4,
+				NumColumns = m_numColumns,
+				NumRows = m_numRows,
 				AllowOutsideTap = true,
 				ShowCloseButton = false,
 				Caption = _("Set Hour"),
 				ColumnWidth = 100,
 				RowHeight = 50,
-				MaxElements = 24,
+				MaxElements = m_maxElements,
 				OnCellClicked = OnCellClicked,
 				OnPopulateCell = OnPopulateCell,
 				Prefab = m_gridPickerCellPrefab
@@ -46,7 +50,9 @@ namespace GuiToolkit
 
 		private void OnPopulateCell(UiGridPicker _gridPicker, int _x, int _y, UiGridPickerCell _cell)
 		{
-_cell.OptionalCaption = $"{_x}/{_y}";
+			int hour = _y * m_numColumns + _x;
+			DateTime time = new DateTime(2000, 1, 1, hour, 0, 0);
+			_cell.OptionalCaption = time.ToShortTimeString().Replace(":00", "");
 		}
 
 		private void OnCellClicked(UiGridPicker _gridPicker, int _x, int _y, UiGridPickerCell _cell)
