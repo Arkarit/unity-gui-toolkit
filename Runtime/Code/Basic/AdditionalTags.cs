6af017b0c81499e6f5cab0d29634feb11600ee63
Heiko Achilles <achilles@funatics.de>
2025-03-24 07:30:06 +0100

    Merge Basic

---
+++ Runtime/Code/Basic/AdditionalTags.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GuiToolkit
+{
+	[ExecuteAlways]
+	public class AdditionalTags : MonoBehaviour
+	{
+		[SerializeField] private List<string> m_Tags = new ();
+		
+		private readonly HashSet<string> m_HashSetTags = new();
+
+		private void Awake()
+		{
+			SetHashSetTags();
+		}
+
+		public bool CompareTag(string _tag)
+		{
+			if (gameObject.CompareTag(_tag))
+				return true;
+			
+			return m_HashSetTags.Contains(_tag);
+		}
+
+		private void OnValidate()
+		{
+			SetHashSetTags();
+		}
+
+		private void SetHashSetTags()
+		{
+			m_HashSetTags.Clear();
+			foreach (var tag in m_Tags)
+				m_HashSetTags.Add(tag);
+		}
+	}
+}
