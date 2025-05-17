ec2344a88284046d5e05413968b0036f6db18ac5
Heiko Achilles <achilles@funatics.de>
2025-05-17 08:30:09 +0200

    Revert "Hopefully fixed loca keys still not found in packages"
    This reverts commit 310f25b687e576e9cd175b85a6453fb310e6f496.

--- Runtime/Code/Loca/LocaProcessor.cs
+++ Runtime/Code/Loca/LocaProcessor.cs
@@ -25,7 +25,7 @@ namespace GuiToolkit
 
 			LocaManager.Instance.Clear();
 
-			EditorAssetUtility.AssetSearchOptions options = new (){Folders = new []{"Assets", "Packages"}};
+			EditorAssetUtility.AssetSearchOptions options = new (){Folders = new []{"Assets", "Packages/de.phoenixgrafik.ui-toolkit"}};
 			
 			EditorUtility.DisplayProgressBar("Processing Loca", "Processing scenes", 0);
 			EditorAssetUtility.FindAllComponentsInAllScenes<ILocaClient>(FoundComponent, options);
