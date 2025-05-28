75ec1c80763dddb2fe995ff0e79faac73555140e
Arkarit <phoenix@phoenixgrafik.de>
2025-05-28 22:16:54 +0200

    Path field and attribute basically works ...although I really don't know why :|

--- Editor/Basic/PathFieldDrawer.cs
+++ Editor/Basic/PathFieldDrawer.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework.Internal;
 using System;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,9 +12,9 @@ namespace GuiToolkit.Editor
 		public override void OnGUI( Rect _rect, SerializedProperty _property, GUIContent _label )
 		{
 			var pathProp = _property.FindPropertyRelative("Path");
-			var thisPathField = (PathField) pathProp.boxedValue;
-			
-			PathFieldAttribute attribute = (PathFieldAttribute)Attribute.GetCustomAttribute(typeof(PathField), typeof(PathFieldAttribute));
+			var thisPathField = (PathField) _property.boxedValue;
+
+			TryGetAttribute(_property, out PathFieldAttribute attribute);
 
 			bool isFolder = attribute?.IsFolder ?? true;
 			bool isRelativeIfPossible = attribute?.IsRelativeIfPossible ?? true;
@@ -27,6 +29,22 @@ namespace GuiToolkit.Editor
 			}
 			
 			pathProp.stringValue = path;
+		}
+
+		public static bool TryGetAttribute<TA>(SerializedProperty _property, out TA _value) 
+			where TA : Attribute
+		{
+			_value = default;
+			var obj = _property.serializedObject.targetObject;
+			if (obj == null)
+				return false;
+
+			FieldInfo field = obj.GetType().GetField(_property.name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+			if (field == null) 
+				return false;
+
+			_value = field.GetCustomAttribute<TA>();
+			return _value != null;
 		}
 	}
 }
