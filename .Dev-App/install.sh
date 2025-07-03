658241d55c747fa1a97116ccabdc93ce4eac9134
Heiko Achilles <achilles@funatics.de>
2025-07-03 10:39:14 +0200

    Removed symlink solution, using local package instead

--- .Dev-App/install.sh
+++
@@ -1,5 +0,0 @@
-#/usr/bin/env bash
-
-# Setup symlinks for the Unity project
-ln -s -v "$(pwd)/../Runtime" "$(pwd)/Unity/Assets/External/unity-gui-toolkit"
-ln -s -v "$(pwd)/../Editor" "$(pwd)/Unity/Assets/External/unity-gui-toolkit-editor"
