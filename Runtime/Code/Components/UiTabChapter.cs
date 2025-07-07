7f85770f979095535a5d3f70bc0121986d27da04
Heiko Achilles <achilles@funatics.de>
2025-07-07 08:41:32 +0200

    Objects to enable and disable depending on text in UiTabChapter

--- Runtime/Code/Components/UiTabChapter.cs
+++ Runtime/Code/Components/UiTabChapter.cs
@@ -8,6 +8,8 @@ namespace GuiToolkit
 		[SerializeField] protected VerticalLayoutGroup m_verticalLayoutGroup;
 		[SerializeField] int m_topPaddingWithText = 91;
 		[SerializeField] int m_topPaddingWithoutText = 30;
+		[SerializeField] GameObject[] m_objectsToEnableWithText;
+		[SerializeField] GameObject[] m_objectsToEnableWithoutText;
 
 		public override string Text
 		{
@@ -15,7 +17,12 @@ namespace GuiToolkit
 			set
 			{
 				base.Text = value;
-				m_verticalLayoutGroup.padding.top = string.IsNullOrEmpty(base.Text) ? m_topPaddingWithoutText : m_topPaddingWithText;
+				bool hasText = !string.IsNullOrEmpty(base.Text);
+				m_verticalLayoutGroup.padding.top = hasText ? m_topPaddingWithText : m_topPaddingWithoutText;
+				foreach (var go in m_objectsToEnableWithText)
+					go.SetActive(hasText);
+				foreach (var go in m_objectsToEnableWithoutText)
+					go.SetActive(!hasText);
 			}
 		}
 	}
