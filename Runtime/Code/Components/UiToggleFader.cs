cbfe3753f8b162d679488d548a5ad7e659328c07
Heiko Achilles <achilles@funatics.de>
2025-07-09 12:16:21 +0200

    Introduce toggle fader (fades canvas group by toggle)

---
+++ Runtime/Code/Components/UiToggleFader.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	[RequireComponent(typeof(CanvasGroup))]
+	public class UiToggleFader : MonoBehaviour
+	{
+		[SerializeField] protected CanvasGroup m_canvasGroup;
+		[SerializeField] protected Toggle m_toggle;
+		[SerializeField] protected bool m_disableAfterFade;
+
+		protected Coroutine m_coroutine;
+		
+		public Toggle Toggle
+		{
+			get
+			{
+				if (m_toggle == null)
+					m_toggle = GetComponentInParent<Toggle>();
+				
+				return m_toggle;
+			}
+		}
+		
+		public CanvasGroup CanvasGroup
+		{
+			get
+			{
+				if (m_canvasGroup == null)
+					m_canvasGroup = GetComponentInParent<CanvasGroup>();
+				
+				return m_canvasGroup;
+			}
+		}
+		
+		float Duration => Toggle.colors.fadeDuration;
+
+		void OnEnable()
+		{
+			Toggle.onValueChanged.AddListener(OnToggleValueChanged);
+			OnToggleValueChanged(Toggle.isOn);
+		}
+
+		void OnDisable()
+		{
+			Toggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+		}
+
+		void OnToggleValueChanged( bool _isOn )
+		{
+			if (m_coroutine != null) 
+				StopCoroutine(m_coroutine);
+			
+			m_coroutine = StartCoroutine(FadeRoutine(_isOn ? 1 : 0));
+		}
+
+		System.Collections.IEnumerator FadeRoutine( float target )
+		{
+			float start = CanvasGroup.alpha;
+			for (float t = 0; t < 1; t += Time.unscaledDeltaTime / Duration)
+			{
+				CanvasGroup.alpha = Mathf.Lerp(start, target, t);
+				yield return null;
+			}
+			
+			CanvasGroup.alpha = target;
+
+			if (m_disableAfterFade)
+			{
+				bool interactable = target > .01f;
+				CanvasGroup.interactable = interactable;
+				CanvasGroup.blocksRaycasts = interactable;
+			}
+		}
+	}
+}
