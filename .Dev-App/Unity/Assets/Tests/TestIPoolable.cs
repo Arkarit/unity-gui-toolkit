6cdcc5be899deaa4a42ef8aed584441951a270d3
Heiko Achilles <achilles@funatics.de>
2025-07-14 08:31:14 +0200

    Tests for pool WIP, failing

---
+++ .Dev-App/Unity/Assets/Tests/TestIPoolable.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace GuiToolkit.Test
+{
+	public class TestIPoolable
+	{
+		private GameObject m_prefab;
+		private UiPool m_pool;
+
+		private class TestPoolable : MonoBehaviour, IPoolable
+		{
+			public int CreatedCount = 0;
+			public int ReleasedCount = 0;
+
+			public void OnPoolCreated() => CreatedCount++;
+			public void OnPoolReleased() => ReleasedCount++;
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			m_prefab = new GameObject("PrefabWithPoolable");
+			m_prefab.AddComponent<TestPoolable>();
+
+			var poolGO = new GameObject("UiPool");
+			m_pool = poolGO.AddComponent<UiPool>();
+			m_pool.m_poolContainer = new GameObject("PoolContainer").transform;
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			Object.DestroyImmediate(m_prefab);
+			Object.DestroyImmediate(m_pool.gameObject);
+		}
+
+		[Test]
+		public void OnPoolCreated_Is_Called_On_GetT()
+		{
+			var instance = m_pool.Get<TestPoolable>(m_prefab.GetComponent<TestPoolable>());
+			Assert.AreEqual(1, instance.CreatedCount);
+		}
+
+		[Test]
+		public void OnPoolReleased_Is_Called_On_ReleaseT()
+		{
+			var instance = m_pool.Get<TestPoolable>(m_prefab.GetComponent<TestPoolable>());
+			m_pool.Release(instance);
+			Assert.AreEqual(1, instance.ReleasedCount);
+		}
+
+		[Test]
+		public void Multiple_Reuse_Calls_OnPoolable_Correctly()
+		{
+			var instance = m_pool.Get<TestPoolable>(m_prefab.GetComponent<TestPoolable>());
+			m_pool.Release(instance);
+			instance = m_pool.Get<TestPoolable>(m_prefab.GetComponent<TestPoolable>());
+			m_pool.Release(instance);
+
+			Assert.AreEqual(2, instance.CreatedCount);
+			Assert.AreEqual(2, instance.ReleasedCount);
+		}
+
+		[Test]
+		public void Get_By_GameObject_Does_Not_Trigger_IPoolable()
+		{
+			var instance = m_pool.Get(m_prefab);
+			var poolable = instance.GetComponent<TestPoolable>();
+
+			Assert.AreEqual(0, poolable.CreatedCount);
+			Assert.AreEqual(0, poolable.ReleasedCount);
+		}
+
+		[Test]
+		public void Releasing_Alien_With_IPoolable_Does_Not_Call_It()
+		{
+			var alien = new GameObject("Alien");
+			var poolable = alien.AddComponent<TestPoolable>();
+
+			m_pool.Release(poolable);
+
+			Assert.AreEqual(0, poolable.ReleasedCount);
+			Object.DestroyImmediate(alien);
+		}
+	}
+}
---
+++ .Dev-App/Unity/Assets/Tests/TestUiPool.cs
@@ -0,0 +1,108 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace GuiToolkit.Test
+{
+	public class TestUiPool
+	{
+		private GameObject m_prefabA;
+		private GameObject m_prefabB;
+		private UiPool m_pool;
+
+		[SetUp]
+		public void SetUp()
+		{
+			m_prefabA = new GameObject("PrefabA");
+			m_prefabB = new GameObject("PrefabB");
+
+			var poolGO = new GameObject("UiPool");
+			m_pool = poolGO.AddComponent<UiPool>();
+			m_pool.m_poolContainer = new GameObject("PoolContainer").transform;
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			Object.DestroyImmediate(m_prefabA);
+			Object.DestroyImmediate(m_prefabB);
+			Object.DestroyImmediate(m_pool.gameObject);
+		}
+
+		[Test]
+		public void Get_And_Release_Works()
+		{
+			var instance = m_pool.Get(m_prefabA);
+			Assert.IsNotNull(instance);
+			Assert.IsTrue(instance.activeSelf);
+
+			m_pool.Release(instance);
+			Assert.IsFalse(instance.activeSelf);
+			Assert.AreEqual(m_pool.m_poolContainer, instance.transform.parent);
+		}
+
+		[Test]
+		public void Instance_Is_Reused()
+		{
+			var instance1 = m_pool.Get(m_prefabA);
+			m_pool.Release(instance1);
+			var instance2 = m_pool.Get(m_prefabA);
+
+			Assert.AreSame(instance1, instance2);
+		}
+
+		[Test]
+		public void Different_Prefabs_Have_Independent_Pools()
+		{
+			var instanceA = m_pool.Get(m_prefabA);
+			var instanceB = m_pool.Get(m_prefabB);
+
+			Assert.AreNotSame(instanceA, instanceB);
+			m_pool.Release(instanceA);
+			m_pool.Release(instanceB);
+
+			var reusedA = m_pool.Get(m_prefabA);
+			var reusedB = m_pool.Get(m_prefabB);
+
+			Assert.AreSame(instanceA, reusedA);
+			Assert.AreSame(instanceB, reusedB);
+		}
+
+		[Test]
+		public void Release_After_Clear_Leads_To_Destroy()
+		{
+			var instance = m_pool.Get(m_prefabA);
+			m_pool.Clear();
+
+			LogAssert.ignoreFailingMessages = true; // optional, um Unity-Warnungen zu unterdrücken
+			Assert.DoesNotThrow(() => m_pool.Release(instance));
+		}
+
+		[Test]
+		public void Release_Unknown_Object_Leads_To_Destroy()
+		{
+			var alien = new GameObject("I do not belong here");
+			Assert.DoesNotThrow(() => m_pool.Release(alien));
+			Object.DestroyImmediate(alien);
+		}
+
+		[Test]
+		public void Destroy_Then_Release_Is_Handled_Gracefully()
+		{
+			var instance = m_pool.Get(m_prefabA);
+			Object.DestroyImmediate(instance);
+			Assert.DoesNotThrow(() => m_pool.Release(instance));
+		}
+
+		[Test]
+		public void Get_After_Clear_Works_Again()
+		{
+			var instance1 = m_pool.Get(m_prefabA);
+			m_pool.Release(instance1);
+			m_pool.Clear();
+
+			var instance2 = m_pool.Get(m_prefabA);
+			Assert.IsNotNull(instance2);
+		}
+	}
+}
