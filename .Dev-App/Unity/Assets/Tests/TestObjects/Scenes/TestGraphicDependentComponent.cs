14dbd604ceedbed5b18f55d3e601841ae79a5f35
Heiko Achilles <achilles@funatics.de>
2025-08-27 17:33:59 +0200

    Fixed could not replace component when dependency on Graphic

---
+++ .Dev-App/Unity/Assets/Tests/TestObjects/Scenes/TestGraphicDependentComponent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Graphic))]
+public class TestGraphicDependentComponent : MonoBehaviour
+{
+	public string m_string;
+	public int m_int;
+	public float m_float;
+}
