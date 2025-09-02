e8d01d9cf159ad51dbb99aba903b953976857326
Heiko Achilles <achilles@funatics.de>
2025-09-02 08:42:51 +0200

    Changed approach DependencyNode heavy WIP

--- Editor/Tools/SafeDeleteWindow/DependencyNode.cs
+++ Editor/Tools/SafeDeleteWindow/DependencyNode.cs
@@ -1,4 +1,4 @@
-// Assets/Editor/SafeDelete/DependencyNode.cs
+﻿// Assets/Editor/SafeDelete/DependencyNode.cs
 
 using System;
 using System.Collections.Generic;
@@ -27,51 +27,144 @@ namespace GuiToolkit.Editor
 		public bool IsInPackages;
 		public DependencyNode Parent;
 		public Object Object;
-		
-		public readonly HashSet<DependencyNode> DirectDependencies = new ();
-		public readonly HashSet<DependencyNode> DirectDependants = new ();
+
+		public readonly HashSet<DependencyNode> DirectDependencies = new();
+		public readonly HashSet<DependencyNode> DirectDependants = new();
 		public DependencyNodeTree Tree;
 
 		public DependencyNode() { } // Deprecated
-		
-		public DependencyNode(Object _object, DependencyNodeTree _tree, DependencyNode _parent)
+
+		public DependencyNode( Object _object, DependencyNodeTree _tree, DependencyNode _parent )
 		{
 			Object = _object;
 			Tree = _tree;
 			Parent = _parent;
-			
+
 			SetProperties(_object);
-			CollectDirectDependencies();
 		}
 
-		private void CollectDirectDependencies()
+		public void CollectDirectDependencies()
 		{
 			if (IsSceneObject)
 			{
 				CollectDependenciesFromSceneObject();
 				return;
 			}
-			
+
 			CollectDependenciesFromAsset();
 		}
 
 		private void CollectDependenciesFromSceneObject()
 		{
-			//TODO
+			var go = Object as GameObject;
+			if (!go)
+			{
+				if (Object is Component comp) go = comp.gameObject;
+				if (!go) return;
+			}
+
+			// 1) Children are structural dependencies (deleting parent deletes children)
+			var t = go.transform;
+			for (int i = 0; i < t.childCount; ++i)
+			{
+				var childGo = t.GetChild(i).gameObject;
+				var childNode = Tree.GetOrCreateNode(childGo, this);
+				Link(this, childNode);
+			}
+
+			// 2) Component object-references (assets and stage objects)
+			foreach (var c in go.GetComponents<Component>())
+			{
+				if (!c) continue;
+
+				try
+				{
+					using (var so = new SerializedObject(c))
+					{
+						var sp = so.GetIterator();
+						while (sp.NextVisible(true))
+						{
+							if (sp.propertyType != SerializedPropertyType.ObjectReference)
+								continue;
+
+							var refObj = sp.objectReferenceValue;
+							if (!refObj) continue;
+
+							// Asset reference?
+							if (AssetDatabase.Contains(refObj))
+							{
+								var path = AssetDatabase.GetAssetPath(refObj);
+								if (string.IsNullOrEmpty(path)) continue;
+								if (path.EndsWith(".cs", StringComparison.OrdinalIgnoreCase)) continue; // ignore scripts
+
+								// Normalize subassets -> main asset
+								var main = AssetDatabase.LoadMainAssetAtPath(path);
+								if (!main) continue;
+
+								var depNode = Tree.GetOrCreateNode(main, this);
+								Link(this, depNode);
+							}
+							else
+							{
+								// Stage-side reference (component or GO)
+								var refGo = (refObj as GameObject) ?? (refObj as Component)?.gameObject;
+								if (!refGo) continue;
+
+								var depNode = Tree.GetOrCreateNode(refGo, this);
+								Link(this, depNode);
+							}
+						}
+					}
+				}
+				catch
+				{
+					// non-serializable editor components etc. – ignore in MVP
+				}
+			}
 		}
-		
+
 		private void CollectDependenciesFromAsset()
 		{
-			//TODO
+			if (string.IsNullOrEmpty(Path)) return;
+
+			// Unity's cached dependency listing. Always normalize to main asset.
+			var deps = AssetDatabase.GetDependencies(Path, true);
+			foreach (var depPath in deps)
+			{
+				if (string.Equals(depPath, Path, StringComparison.Ordinal))
+					continue; // skip self
+
+				var main = AssetDatabase.LoadMainAssetAtPath(depPath);
+				if (!main) continue;
+
+				var ext = System.IO.Path.GetExtension(depPath);
+				if (string.Equals(ext, ".cs", StringComparison.OrdinalIgnoreCase))
+					continue; // ignore scripts
+
+				var depNode = Tree.GetOrCreateNode(main, this);
+				Link(this, depNode);
+			}
 		}
 		
 
-		private void SetProperties(Object _object)
+		// Small helper to wire the edge both ways and rely on HashSet dedup.
+		private static void Link( DependencyNode from, DependencyNode to )
+		{
+			if (to == null || from == null) 
+				return;
+			if (ReferenceEquals(from, to))
+				return;
+
+			from.DirectDependencies.Add(to);
+			to.DirectDependants.Add(from);
+		}
+
+		private void SetProperties( Object _object )
 		{
 			// Normalize components to owning GameObject
 			if (_object is Component comp)
 				_object = comp.gameObject;
-			
+
 			Name = _object.name;
 			TypeName = _object.GetType().Name;
 			Goid = GlobalObjectId.GetGlobalObjectIdSlow(_object);
@@ -87,7 +180,7 @@ namespace GuiToolkit.Editor
 				IsInPackages = Path.StartsWith("Packages", StringComparison.OrdinalIgnoreCase);
 				return;
 			}
-			
+
 			IsSceneObject = true;
 			Guid = string.Empty;
 			LocalId = 0;
--- Editor/Tools/SafeDeleteWindow/DependencyNodeTree.cs
+++ Editor/Tools/SafeDeleteWindow/DependencyNodeTree.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -9,18 +10,80 @@ namespace GuiToolkit.Editor
 	public class DependencyNodeTree
 	{
 		[SerializeField] private DependencyNode m_root;
-		[SerializeField] private readonly HashSet<DependencyNode> m_dependencies = new ();
-		[SerializeField] private readonly HashSet<DependencyNode> m_dependendants = new ();
-		
+		[SerializeField] private readonly HashSet<DependencyNode> m_allNodes = new();
+		private readonly Dictionary<string, DependencyNode> m_byKey = new(StringComparer.Ordinal);
+
 		public DependencyNode Root
 		{
 			get => m_root;
-			set => m_root = value;
+			private set => m_root = value;
 		}
 
-		public DependencyNodeTree(Object _object)
+		public DependencyNodeTree( Object _object )
 		{
-			m_root = new DependencyNode(_object, this, null);
+			Root = BuildFromRoot(_object);
+		}
+
+		
+		// stable key: Stage -> GOID; Asset -> GUID (Main-Asset-Policy)
+		private static string MakeKey( Object _object )
+		{
+			if (!_object) return null;
+
+			// Normalize: components -> GameObject
+			if (_object is Component c) _object = c.gameObject;
+
+			var path = AssetDatabase.GetAssetPath(_object);
+			if (!string.IsNullOrEmpty(path))
+			{
+				// Normalize subassets -> main asset
+				var main = AssetDatabase.LoadMainAssetAtPath(path) ?? _object;
+				AssetDatabase.TryGetGUIDAndLocalFileIdentifier(main, out var guid, out _);
+				return guid; // main asset key
+			}
+
+			var goid = GlobalObjectId.GetGlobalObjectIdSlow(_object);
+			return goid.ToString(); // stage key
+		}
+
+		public DependencyNode GetOrCreateNode( Object _obj, DependencyNode _parent )
+		{
+			var key = MakeKey(_obj);
+			if (string.IsNullOrEmpty(key)) return null;
+
+			if (m_byKey.TryGetValue(key, out var existing))
+				return existing;
+
+			var node = new DependencyNode(_obj, this, _parent);
+			m_byKey[key] = node;
+			m_allNodes.Add(node);
+			return node;
+		}
+		
+		public DependencyNode BuildFromRoot( Object _root )
+		{
+			if (_root is Component component)
+				_root = component.gameObject;
+			
+			var visited = new HashSet<string>(StringComparer.Ordinal);
+			var stack = new Stack<DependencyNode>();
+			var rootNode = GetOrCreateNode(_root, null);
+			stack.Push(rootNode);
+
+			while (stack.Count > 0)
+			{
+				var n = stack.Pop();
+				var key = MakeKey(n.Object);
+				if (!visited.Add(key)) continue;
+
+				// fill direct deps now
+				n.CollectDirectDependencies();
+
+				foreach (var d in n.DirectDependencies)
+					stack.Push(d);
+			}
+			
+			return rootNode;
 		}
 	}
 }
