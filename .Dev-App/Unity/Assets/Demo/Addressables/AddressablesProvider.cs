0a98d759e2b45a5bf8b42c841cc56c41cf7053d9
Heiko Achilles <achilles@funatics.de>
2025-09-09 16:07:01 +0200

    Continue Asset Loading heavy WIP

--- .Dev-App/Unity/Assets/Demo/Addressables/AddressablesProvider.cs
+++ .Dev-App/Unity/Assets/Demo/Addressables/AddressablesProvider.cs
@@ -8,12 +8,14 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 using Object = UnityEngine.Object;
 using Addressables = UnityEngine.AddressableAssets.Addressables;
 using AssetReference = UnityEngine.AddressableAssets.AssetReference;
-using AssetReferenceGameObject = UnityEngine.AddressableAssets.AssetReferenceGameObject;
 
 public sealed class AddressableInstanceHandle : IInstanceHandle
 {
-	public GameObject Instance { get; }
+	private bool m_isReleased;
 	private AsyncOperationHandle<GameObject> m_handle;
+
+	public GameObject Instance { get; private set; }
+
 	public AddressableInstanceHandle( AsyncOperationHandle<GameObject> _handle )
 	{
 		m_handle = _handle;
@@ -22,100 +24,223 @@ public sealed class AddressableInstanceHandle : IInstanceHandle
 
 	public void Release()
 	{
-		Addressables.ReleaseInstance(Instance);
+		if (m_isReleased)
+			return;
+
+		m_isReleased = true;
+
+		if (Instance)
+		{
+			Addressables.ReleaseInstance(Instance);
+			Instance = null;
+			return;
+		}
+
+		if (m_handle.IsValid())
+			Addressables.Release(m_handle);
 	}
 }
 
 public sealed class AddressableAssetHandle<T> : IAssetHandle<T> where T : Object
 {
-	public T Asset => Handle.Result;
-	public AssetKey Key { get; }
-	public bool IsLoaded => Asset != null;
-	public void Release()
-	{
-		if ( Handle.IsValid())
-			Addressables.Release(Handle);
-	}
+	private bool m_isReleased;
 
+	public AssetKey Key { get; }
 	public AsyncOperationHandle<T> Handle;
+
 	public AddressableAssetHandle( AsyncOperationHandle<T> _handle, AssetKey _key )
 	{
 		Handle = _handle;
 		Key = _key;
 	}
+
+	public bool IsLoaded => Handle.IsValid() && Handle.Status == AsyncOperationStatus.Succeeded;
+
+	public T Asset => (IsLoaded && !m_isReleased) ? Handle.Result : null;
+
+	public void Release()
+	{
+		if (m_isReleased)
+			return;
+
+		m_isReleased = true;
+
+		if (Handle.IsValid())
+			Addressables.Release(Handle);
+	}
 }
 
 public sealed class AddressablesProvider : IAssetProvider
 {
-	public async Task<IInstanceHandle> InstantiateAsync( object _key, Transform _parent = null, CancellationToken _cancellationToken = default )
+	public async Task<IInstanceHandle> InstantiateAsync
+	(
+		object _key,
+		Transform _parent = null,
+		CancellationToken _cancellationToken = default
+	)
 	{
-		var key = NormalizeKey<GameObject>(_key);
-		var handle = Load<GameObject>(key, _cancellationToken);
-		await handle.Task;
-		return new AddressableInstanceHandle(handle);
-	}
+		var assetKey = NormalizeKey<GameObject>(_key);
 
-	public async Task<IAssetHandle<T>> LoadAssetAsync<T>( object _key, CancellationToken _cancellationToken = default ) where T : Object
-	{
-		var key = NormalizeKey<T>(_key);
-		
-		var handle = Load<T>(key, _cancellationToken);
-		await handle.Task;
-		return new AddressableAssetHandle<T>(handle, key);
-	}
+		if (!assetKey.TryGetValue("addr:", out string addr))
+			throw new Exception($"AddressablesProvider.InstantiateAsync: Unsupported key '{assetKey.Id}' (expected 'addr:...').");
 
-	public void Release<T>( IAssetHandle<T> _handle ) where T : Object => _handle.Release();
-	public void Release(IInstanceHandle _handle) => _handle.Release();
+		AsyncOperationHandle<GameObject> handle = default;
 
-	public async Task PreloadAsync( IEnumerable<object> keysOrLabels, CancellationToken ct = default )
-	{
-		foreach (var keyOrLabel in keysOrLabels)
+		try
 		{
-			var handle = Addressables.LoadAssetAsync<GameObject>(keyOrLabel);
+			handle = Addressables.InstantiateAsync(addr, _parent);
+
 			await handle.Task;
-			Addressables.Release(handle);
+
+			if (_cancellationToken.IsCancellationRequested)
+			{
+				if (handle.IsValid() && handle.Status == AsyncOperationStatus.Succeeded && handle.Result)
+					Addressables.ReleaseInstance(handle.Result);
+				else if (handle.IsValid())
+					Addressables.Release(handle);
+
+				_cancellationToken.ThrowIfCancellationRequested();
+			}
+
+			if (!handle.IsValid() || handle.Status != AsyncOperationStatus.Succeeded || !handle.Result)
+			{
+				if (handle.IsValid())
+					Addressables.Release(handle);
+
+				throw new Exception($"InstantiateAsync failed for '{assetKey.Id}' (status {handle.Status}).");
+			}
+
+			return new AddressableInstanceHandle(handle);
+		}
+		catch
+		{
+			if (handle.IsValid())
+			{
+				if (handle.Status == AsyncOperationStatus.Succeeded && handle.Result)
+					Addressables.ReleaseInstance(handle.Result);
+				else
+					Addressables.Release(handle);
+			}
+			throw;
 		}
 	}
 
-	public void ReleaseUnused() { /* optional trimming */ }
+	public async Task<IAssetHandle<T>> LoadAssetAsync<T>
+	(
+		object _key,
+		CancellationToken _cancellationToken = default
+	) where T : Object
+	{
+		var assetKey = NormalizeKey<T>(_key);
+
+		if (!assetKey.TryGetValue("addr:", out string addr))
+			throw new Exception($"AddressablesProvider.LoadAssetAsync: Unsupported key '{assetKey.Id}' (expected 'addr:...').");
+
+		AsyncOperationHandle<T> handle = default;
+
+		try
+		{
+			handle = Addressables.LoadAssetAsync<T>(addr);
+
+			await handle.Task;
+
+			if (_cancellationToken.IsCancellationRequested)
+			{
+				if (handle.IsValid())
+					Addressables.Release(handle);
+
+				_cancellationToken.ThrowIfCancellationRequested();
+			}
+
+			if (!handle.IsValid() || handle.Status != AsyncOperationStatus.Succeeded)
+			{
+				if (handle.IsValid())
+					Addressables.Release(handle);
+
+				throw new Exception($"LoadAssetAsync<{typeof(T).Name}> failed for '{assetKey.Id}' (status {handle.Status}).");
+			}
+
+			return new AddressableAssetHandle<T>(handle, assetKey);
+		}
+		catch
+		{
+			if (handle.IsValid())
+				Addressables.Release(handle);
+			throw;
+		}
+	}
+
+	public void Release<T>( IAssetHandle<T> _handle ) where T : Object
+	{
+		_handle?.Release();
+	}
+
+	public void Release( IInstanceHandle _handle )
+	{
+		_handle?.Release();
+	}
+
+	public async Task PreloadAsync
+	(
+		IEnumerable<object> _keysOrLabels,
+		CancellationToken _cancellationToken = default
+	)
+	{
+		foreach (var raw in _keysOrLabels)
+		{
+			if (_cancellationToken.IsCancellationRequested)
+				break;
+
+			var key = NormalizeKey<GameObject>(raw);
+			if (!key.TryGetValue("addr:", out string addr))
+				continue;
+
+			var h = Addressables.LoadAssetAsync<GameObject>(addr);
+			try
+			{
+				await h.Task;
+			}
+			finally
+			{
+				if (h.IsValid())
+					Addressables.Release(h);
+			}
+		}
+	}
+
+	public void ReleaseUnused()
+	{
+		// optional trimming
+	}
 
 	public AssetKey NormalizeKey<T>( object _key ) where T : Object
 	{
 		if (_key is AssetKey assetKey)
 		{
 			if (!ReferenceEquals(assetKey.Provider, this))
-				throw new InvalidOperationException($"Attempt to use Key designed for Asset provider '{assetKey.Provider.GetType().Name}' with '{GetType().Name}'");
+				throw new InvalidOperationException
+				(
+					$"Attempt to use Key designed for Asset provider '{assetKey.Provider?.GetType().Name}' with '{GetType().Name}'"
+				);
 
 			return assetKey;
 		}
 
-		// Direct AssetReference
 		if (_key is AssetReference assetReference)
 		{
-			// RuntimeKey is the official, stable ID for Addressables
-			string runtimeKey = assetReference.RuntimeKey.ToString();
-			return new AssetKey(this, $"addr:{runtimeKey}", typeof(T));
+			var runtimeKey = assetReference.RuntimeKey.ToString();
+			if (!runtimeKey.StartsWith("addr:", StringComparison.Ordinal))
+				runtimeKey = $"addr:{runtimeKey}";
+
+			return new AssetKey(this, runtimeKey, typeof(T));
 		}
 
-		// String key/label (Addressables supports both)
-		if (_key is string key)
-			return new AssetKey(this, $"addr:{key}", typeof(T));
+		if (_key is string s)
+		{
+			var id = s.StartsWith("addr:", StringComparison.Ordinal) ? s : $"addr:{s}";
+			return new AssetKey(this, id, typeof(T));
+		}
 
 		return new AssetKey(this, $"unknown:{_key}", typeof(T));
-	}
-
-	public AsyncOperationHandle<T> Load<T>( AssetKey _assetKey, CancellationToken _cancellationToken ) where T : Object
-	{
-		AsyncOperationHandle<T> result = new AsyncOperationHandle<T>();
-
-		if (_assetKey.TryGetValue("addr:", out string addr))
-			result = Addressables.LoadAssetAsync<T>(addr);
-
-		_cancellationToken.ThrowIfCancellationRequested();
-
-		if (!result.IsValid())
-			throw new Exception($"Could not load asset: '{_assetKey.Id}'");
-
-		return result;
 	}
 }
--- Runtime/Code/Basic/AssetHandling/DefaultAssetProvider.cs
+++ Runtime/Code/Basic/AssetHandling/DefaultAssetProvider.cs
@@ -15,16 +15,20 @@ namespace GuiToolkit.AssetHandling
 	public sealed class DefaultInstanceHandle : IInstanceHandle
 	{
 		public GameObject Instance { get; private set; }
-		public void Release()
+
+		public DefaultInstanceHandle( GameObject _object )
 		{
-			if (Instance)
-			{
-				Object.Destroy(Instance);
-				Instance = null;
-			}
+			Instance = _object;
 		}
 
-		public DefaultInstanceHandle( GameObject _object ) { Instance = _object; }
+		public void Release()
+		{
+			if (!Instance)
+				return;
+
+			Object.Destroy(Instance);
+			Instance = null;
+		}
 	}
 
 	public sealed class DefaultAssetHandle<T> : IAssetHandle<T> where T : Object
@@ -32,15 +36,12 @@ namespace GuiToolkit.AssetHandling
 		public T Asset { get; }
 		public AssetKey Key { get; }
 		public bool IsLoaded => Asset != null;
-		public void Release()
-		{
-			if (Asset)
-				Object.Destroy(Asset);
-		}
+
+		public void Release() { }
 
 		public DefaultAssetHandle( T _asset, AssetKey _key )
 		{
-			Asset = _asset; 
+			Asset = _asset;
 			Key = _key;
 		}
 	}
@@ -51,34 +52,48 @@ namespace GuiToolkit.AssetHandling
 		(
 			object _key,
 			Transform _parent = null,
-			CancellationToken _cancellationToken = default 
+			CancellationToken _cancellationToken = default
 		)
 		{
 			var assetKey = NormalizeKey<GameObject>(_key);
-			GameObject prefab = (GameObject) Load(assetKey, _cancellationToken);
+			var prefab = (GameObject) Load(assetKey, _cancellationToken);
 
-			// Keep method truly async-friendly
 			await Task.Yield();
+			_cancellationToken.ThrowIfCancellationRequested();
 
 			var instance = Object.Instantiate(prefab, _parent);
 			return new DefaultInstanceHandle(instance);
 		}
 
-		public async Task<IAssetHandle<T>> LoadAssetAsync<T>( object _key, CancellationToken _cancellationToken = default ) where T : Object
+		public async Task<IAssetHandle<T>> LoadAssetAsync<T>
+		(
+			object _key,
+			CancellationToken _cancellationToken = default
+		) where T : Object
 		{
 			var assetKey = NormalizeKey<T>(_key);
-			T obj = (T) Load(assetKey, _cancellationToken);
-			
-			// Keep method truly async-friendly
+			var obj = (T) Load(assetKey, _cancellationToken);
+
 			await Task.Yield();
-			
+
 			return new DefaultAssetHandle<T>(obj, assetKey);
 		}
 
-		public void Release<T>( IAssetHandle<T> _handle ) where T : Object => _handle.Release();
-		public void Release(IInstanceHandle _handle) => _handle.Release();
-		
-		public Task PreloadAsync( IEnumerable<object> _keysOrLabels, CancellationToken _cancellationToken = default ) => Task.CompletedTask;
+		public void Release<T>( IAssetHandle<T> _handle ) where T : Object
+		{
+			_handle?.Release(); // no-op
+		}
+
+		public void Release( IInstanceHandle _handle )
+		{
+			_handle?.Release();
+		}
+
+		public Task PreloadAsync
+		(
+			IEnumerable<object> _keysOrLabels,
+			CancellationToken _cancellationToken = default
+		) => Task.CompletedTask;
 
 		public void ReleaseUnused() { }
 
@@ -87,41 +102,53 @@ namespace GuiToolkit.AssetHandling
 			if (_key is AssetKey assetKey)
 			{
 				if (!ReferenceEquals(assetKey.Provider, this))
-					throw new InvalidOperationException($"Attempt to use Key designed for Asset provider '{assetKey.Provider.GetType().Name}' with '{GetType().Name}'");
+					throw new InvalidOperationException
+					(
+						$"Attempt to use Key designed for Asset provider '{assetKey.Provider?.GetType().Name}' with '{GetType().Name}'"
+					);
 
 				return assetKey;
 			}
 
-			// Prefab reference directly assigned
 			if (_key is Object obj)
 			{
 #if UNITY_EDITOR
 				if (!EditorUtility.IsPersistent(obj))
-					throw new InvalidOperationException($"Invalid key object '{obj.name}': Only persistent objects can be used as keys");
+					throw new InvalidOperationException($"Invalid key object '{obj.name}': Only persistent assets can be used as keys");
+
+				string path = AssetDatabase.GetAssetPath(obj);
+				if (!string.IsNullOrEmpty(path))
+				{
+					string guidStr = AssetDatabase.AssetPathToGUID(path);
+					if (!string.IsNullOrEmpty(guidStr))
+						return new AssetKey(this, $"guid:{guidStr}", typeof(T));
+				}
+
+				throw new InvalidOperationException($"Could not derive GUID for asset object '{obj.name}'.");
+#else
+				throw new InvalidOperationException("Object keys are not supported at runtime. Use a 'res:' path instead.");
 #endif
-				return new AssetKey(this, obj.GetInstanceID().ToString(), typeof(T));
 			}
 
-			// Resources.Load() style: use path as stable Id
-			if (_key is string path)
-				return new AssetKey(this, $"res:{path}", typeof(T));
+			if (_key is string pathStr)
+				return new AssetKey(this, pathStr.StartsWith("res:", StringComparison.Ordinal) ? pathStr : $"res:{pathStr}", typeof(T));
 
 #if UNITY_EDITOR
-			// Editor only: GUID normalized as string
 			if (_key is Guid guid)
 				return new AssetKey(this, $"guid:{guid:N}", typeof(T));
 #endif
 
-			// Fallback: ToString of whatever came in
 			return new AssetKey(this, $"unknown:{_key}", typeof(T));
 		}
-		
-		public Object Load(AssetKey _assetKey, CancellationToken _cancellationToken)
+
+		public Object Load( AssetKey _assetKey, CancellationToken _cancellationToken )
 		{
 			Object result = null;
 
 			if (_assetKey.TryGetValue("res:", out string resourcePath))
+			{
 				result = Resources.Load(resourcePath, _assetKey.Type);
+			}
 
 #if UNITY_EDITOR
 			else if (_assetKey.TryGetValue("guid:", out string guid))
@@ -135,8 +162,11 @@ namespace GuiToolkit.AssetHandling
 			_cancellationToken.ThrowIfCancellationRequested();
 
 			if (!result)
-				throw new Exception($"Could not load asset: '{_assetKey.Id}'");
-			
+				throw new Exception
+				(
+					$"DefaultAssetProvider.Load failed: id='{_assetKey.Id}', type='{_assetKey.Type?.Name ?? "null"}', provider='{GetType().Name}'."
+				);
+
 			return result;
 		}
 	}
