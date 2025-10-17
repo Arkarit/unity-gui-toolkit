d2b21334fdf9f71f21636593d2b16756ecd0297c
Heiko Achilles <achilles@funatics.de>
2025-10-18 01:52:48 +0200

    Cont Test dialog for animations

--- .Dev-App/Unity/Assets/Demo/ExampleAnimations.cs
+++ .Dev-App/Unity/Assets/Demo/ExampleAnimations.cs
@@ -8,7 +8,9 @@ public class ExampleAnimations : UiView
 	public UiButton m_closeButton;
 	public UiButton m_btPlayChildrenAnimation;
 	public UiButton m_btPlayChildrenAnimationBackwards;
+	public UiButton m_btToggleAutoChildrenAnimation;
 	public UiSimpleAnimationBase m_childrenAnimation;
+	public GameObject m_autoChildrenParent;
 	
 	public override bool AutoDestroyOnHide => true;
 	public override bool Poolable => true;
@@ -21,5 +23,6 @@ public class ExampleAnimations : UiView
 		m_closeButton.OnClick.AddListener(() => Hide());
 		m_btPlayChildrenAnimation.OnClick.AddListener(() => m_childrenAnimation.Play());
 		m_btPlayChildrenAnimationBackwards.OnClick.AddListener(() => m_childrenAnimation.Play(true));
+		m_btToggleAutoChildrenAnimation.OnClick.AddListener(() => m_autoChildrenParent.SetActive(!m_autoChildrenParent.activeSelf));
 	}
 }
