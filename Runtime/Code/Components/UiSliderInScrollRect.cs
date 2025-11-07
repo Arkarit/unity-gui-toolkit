4897110a35598b66424502678d0ed629912df1a9
Heiko Achilles <achilles@funatics.de>
2025-11-07 10:07:46 +0100

    Introduced new class UiScrollRectInScrollRect Similar to UiSliderInScrollRect

--- Runtime/Code/Components/UiSliderInScrollRect.cs
+++ Runtime/Code/Components/UiSliderInScrollRect.cs
@@ -1,132 +1,54 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using System;
-using System.Collections;
-
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
+using UnityEngine.UI;
 
 namespace GuiToolkit
 {
-	/// \brief Helper component for horizontal sliders in vertical scroll rects and vice versa
+	/// \brief Helper component for horizontal sliders in vertical scroll rects and vice versa.
 	/// 
-	/// Usually, horizontal sliders in vertical layouts (and vice versa) cause a lot of issues UX-wise.
-	/// If you want to scroll the scroll rect up or down, but happen to tap on a slider, you move the slider instead, which is super annoying.
-	/// UiSliderInScrollRect helps against this. It should reside on a Graphic covering the whole slider.
-	/// It catches all necessary mouse/drag events, evaluates if the drag operation is horizontal or vertical, and forwards the
-	/// events to the matching component - either a slider or a scroll rect.
-	/// 
-	/// Note that this component destroys its own game object if the Slider and ScrollRect member vars haven't been set.
-	/// For the ScrollRect member, it tries to find a scroll rect in parent first.
-	/// This makes it easy to make it a part of a slider prefab, since otherwise it would always block all mouse/drag actions regarding that slider.
-	public class UiSliderInScrollRect : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+	/// Split version based on UiDragRouterBase.
+	public class UiSliderInScrollRect : UiAbstractDragRouter
 	{
-		private const float DRAG_DETECTION_TIME = 0.2f;
+		[SerializeField][Mandatory] private Slider m_slider;
+		[SerializeField][Mandatory] private ScrollRect m_scrollRect;
 
-		public Slider m_slider;
-		public ScrollRect m_scrollRect;
-
-		private PointerEventData m_eventData;
-		private ETriState m_isHorizontal;
-		private IBeginDragHandler m_beginDragHandler;
-		private IDragHandler m_dragHandler;
-		private IEndDragHandler m_endDragHandler;
-		private IPointerDownHandler m_pointerDownHandler;
-		private bool m_handlersDetermined;
-		private bool m_wasDragged;
-
-		public void Start()
+		protected override bool TryResolveDependencies()
 		{
 			if (m_slider == null)
-			{
-				Destroy(gameObject);
-				return;
-			}
+				return false;
 
 			if (m_scrollRect == null)
 				m_scrollRect = GetComponentInParent<ScrollRect>();
 
-			if (m_scrollRect == null)
-				Destroy(gameObject);
+			return m_scrollRect != null;
 		}
 
-		public void OnBeginDrag( PointerEventData _eventData )
+		protected override MonoBehaviour GetPrimaryTarget()
 		{
-			m_eventData = _eventData.ShallowClone();
-			m_handlersDetermined = false;
-			m_wasDragged = true;
+			return m_slider;
 		}
 
-		public void OnDrag( PointerEventData _eventData )
+		protected override MonoBehaviour GetSecondaryTarget()
 		{
-			if (m_eventData != null)
-				m_isHorizontal = EvalHorizontal(m_eventData, _eventData);
-
-			if (m_isHorizontal == ETriState.Indeterminate)
-				return;
-
-			if (!m_handlersDetermined)
-			{
-				m_handlersDetermined = true;
-				bool isDragHorizontal = m_isHorizontal == ETriState.True;
-				bool isSliderHorizontal = m_slider.direction == Slider.Direction.LeftToRight || m_slider.direction == Slider.Direction.RightToLeft;
-				bool isSliderUsed = isDragHorizontal == isSliderHorizontal;
-				MonoBehaviour mb = isSliderUsed ? (MonoBehaviour) m_slider : (MonoBehaviour) m_scrollRect;
-				SetHandlers(mb);
-			}
-
-			if (m_beginDragHandler != null)
-			{
-				m_beginDragHandler.OnBeginDrag(m_eventData);
-				m_beginDragHandler = null;
-			}
-
-			m_eventData = null;
-
-			if (m_dragHandler != null)
-				m_dragHandler.OnDrag(_eventData);
+			return m_scrollRect;
 		}
 
-		public void OnEndDrag( PointerEventData _eventData )
+		protected override bool IsPrimaryHorizontal()
 		{
-			if (m_endDragHandler != null)
-				m_endDragHandler.OnEndDrag(_eventData);
+			var dir = m_slider.direction;
+
+			if (dir == Slider.Direction.LeftToRight)
+				return true;
+
+			if (dir == Slider.Direction.RightToLeft)
+				return true;
+
+			return false;
 		}
 
-		public void OnPointerDown( PointerEventData _eventData )
+		protected override void OnPrimaryPointerDown(PointerEventData _eventData)
 		{
-			StartCoroutine(DelayedOnPointerDown(_eventData.ShallowClone()));
-		}
-
-		private void SetHandlers( MonoBehaviour _monoBehaviour )
-		{
-			m_beginDragHandler = _monoBehaviour is IBeginDragHandler ? (IBeginDragHandler)_monoBehaviour : null;
-			m_dragHandler = _monoBehaviour is IDragHandler ? (IDragHandler)_monoBehaviour : null;
-			m_endDragHandler = _monoBehaviour is IEndDragHandler ? (IEndDragHandler)_monoBehaviour : null;
-			m_pointerDownHandler = _monoBehaviour is IPointerDownHandler ? (IPointerDownHandler)_monoBehaviour : null;
-		}
-
-		private static ETriState EvalHorizontal( PointerEventData _before, PointerEventData _after )
-		{
-			Vector2 delta = _after.position - _before.position;
-			Vector2 deltaAbs = new Vector2(Mathf.Abs(delta.x), Mathf.Abs(delta.y));
-			if (deltaAbs.magnitude < 1)
-				return ETriState.Indeterminate;
-
-			return deltaAbs.x > deltaAbs.y ? ETriState.True : ETriState.False;
-		}
-
-		private IEnumerator DelayedOnPointerDown(PointerEventData _eventData)
-		{
-			yield return new WaitForSecondsRealtime(DRAG_DETECTION_TIME);
-
-			if (!m_wasDragged)
-				m_slider.OnPointerDown(_eventData);
-			m_wasDragged = false;
+			m_slider.OnPointerDown(_eventData);
 		}
 	}
-
 }
