9174f464cdc74466654057f7675fc320246ad02e
Heiko Achilles <achilles@funatics.de>
2025-11-08 01:08:36 +0100

    Fixed compile issue

--- Runtime/Code/Basic/Attributes/ReadOnlyAttribute.cs
+++ Runtime/Code/Basic/Attributes/ReadOnlyAttribute.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 #if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace GuiToolkit
 {
+#if UNITY_EDITOR
 	[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
 	public class ReadOnlyDrawer : PropertyDrawer
 	{
