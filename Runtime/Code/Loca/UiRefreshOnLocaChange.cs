3e452dd662804e451ddf97b375e395c54731323f
Heiko Achilles <achilles@funatics.de>
2026-04-15 12:17:23 +0200

    BOTW-1037: Add UiRefreshOnLocaChange component

---
+++ Runtime/Code/Loca/UiRefreshOnLocaChange.cs
@@ -0,0 +1,25 @@
+using UnityEngine.UI;
+
+namespace GuiToolkit
+{
+	/// <summary>
+	/// Invalidates all child <see cref="LayoutGroup"/> components and force-rebuilds
+	/// the layout hierarchy whenever the active language changes.
+	/// Attach this to any UI container whose dimensions depend on translated text length.
+	/// </summary>
+	public class UiRefreshOnLocaChange : UiThing
+	{
+		protected override bool NeedsLanguageChangeCallback => true;
+
+		protected override void OnLanguageChanged( string _languageId )
+		{
+			base.OnLanguageChanged(_languageId);
+
+			var layoutGroups = GetComponentsInChildren<LayoutGroup>(true);
+			foreach (var layoutGroup in layoutGroups)
+				LayoutRebuilder.MarkLayoutForRebuild((RectTransform) layoutGroup.transform);
+
+			LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) transform);
+		}
+	}
+}
